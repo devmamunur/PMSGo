39a0b4e531a779f8cf9ad2da0adb1cd8b608df2c
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-04-28 23:18:28 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = require('./src/route/api');
+const app = new express();
+
+//Body Parser Implementation
+bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
+//Security Middleware
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+
+//Database
+const mongoose = require('mongoose');
+const path = require("path");
+
+//Security Middleware Implement
+app.use(cors());
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+
+
+//Rate Limiter
+const limiter = rateLimit({windowMs: 15 * 60 * 100, max: 3000})
+
+//Database
+let URI = "mongodb://127.0.0.1:27017/task_manager";
+let OPTION = {user: '', pass: '', autoIndex: true};
+mongoose.connect(URI, OPTION).then((res) => {
+    console.log("Database Connection Successfully!");
+}).catch((err) => {
+    console.log("Database Connection Failed!");
+})
+
+
+//Managing Front End Routing
+app.use(express.static('client/build'))
+app.get("*",function (req,res){
+    req.sendFile(path.resolve(__dirname,'client','build','index.html'))
+})
+
+
+// Managing BackEnd API Routing
+app.use("/api/v1", router)
+
+
+module.exports = app;
+
+
---
+++ src/route/api.js
@@ -0,0 +1,8 @@
+// Import dependencies
+const express=require('express');
+const router=express.Router();
+
+
+
+
+module.exports=router;
