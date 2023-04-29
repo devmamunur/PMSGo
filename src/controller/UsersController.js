19eb520cebff8ac9f87e69068ac4dd9ed8df6167
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-04-29 16:35:40 +0600

    Create login request and route

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,7 +1,46 @@
 const UsersModel = require("../models/UsersModel")
+const jwt = require("jsonwebtoken")
 
 // Registration
 exports.registration=(req, res) => {
+
+    const reqBody = req.body
+    UsersModel.create(reqBody).then((document) => {
+        res.status(200).json({success : true, data : document});
+    }).catch((error) => {
+        res.status(400).json({success : false, data : error});
+    })
+}
+
+// Login
+exports.login = (req, res) => {
+    const reqBody = req.body;
+    UsersModel.aggregate([
+        { $match: reqBody },
+        {
+            $project: {
+                _id: 0,
+                email: 1,
+                firstName: 1,
+                lastName: 1,
+                mobile: 1,
+                photo: 1,
+            },
+        },
+    ]).then((data) => {
+        if(data.length > 0){
+            let Payload = { exp : Math.floor(Date.now() / 1000) + (24*60*60), data : data[0]['email']};
+            let token = jwt.sign(Payload, 'Secret123');
+            res.status(200).json({success : true, token : token, data : data[0]});
+        }else {
+            res.status(401).json({success : false, data : "Unauthorized"});
+        }
+    }).catch((error)=> {
+        res.status(400).json({success : false, data : error});
+    });
+}
+// Profile Update
+exports.profileUpdate=(req, res) => {
     const reqBody = req.body
     UsersModel.create(reqBody).then((document) => {
         res.status(200).json({success : true, data : document});
--- src/route/api.js
+++ src/route/api.js
@@ -2,9 +2,14 @@
 const express=require('express');
 const router=express.Router();
 
+// Import Controller
 const UsersController = require("../controller/UsersController")
 
+
+// Route Path
 router.post("/registration", UsersController.registration);
+router.post("/login", UsersController.login);
+router.post("/profile-update", UsersController.profileUpdate);
 
 
 module.exports=router;
