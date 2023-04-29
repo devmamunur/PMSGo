266cc96a6a83844db667036fe55ffcd75b9ffcee
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-04-29 23:05:10 +0600

    remove old client folder

--- app.js
+++ app.js
@@ -39,13 +39,6 @@ mongoose.connect(URI, OPTION).then((res) => {
 })
 
 
-//Managing Front End Routing
-// app.use(express.static('client/build'))
-// app.get("*",function (req,res){
-//     req.sendFile(path.resolve(__dirname,'client','build','index.html'))
-// })
-
-
 // Managing BackEnd API Routing
 app.use("/api/v1", router)
 
