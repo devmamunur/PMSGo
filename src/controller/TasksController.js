d385eee20f50e91030ec751666e4861f970c7975
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-04-29 17:51:22 +0600

    Made Task Model Schema.  Task Create Done

---
+++ src/controller/TasksController.js
@@ -0,0 +1,39 @@
+const TasksModel = require("../models/TasksModel")
+
+
+// Task Creat
+exports.createTask = (req, res) => {
+    let reqBody = req.body
+    reqBody.email = req.headers["email"];
+
+    TasksModel.create(reqBody).then((document) => {
+        res.status(200).json({success : true, data : document});
+    }).catch((error) => {
+        res.status(400).json({success : false, data : error});
+    })
+}
+
+// Task Delete
+exports.deleteTask = (req, res) => {
+    const id = req.params.id
+    const query = {_id : id}
+
+    TasksModel.deleteOne(query).then((document) => {
+        res.status(200).json({success : true, data : document});
+    }).catch((error) => {
+        res.status(400).json({success : false, data : error});
+    })
+}
+
+// Task Status Update
+exports.updateTaskStatus = (req, res) => {
+    const id = req.params.id
+    const status = req.params.status;
+    const query = {_id : id}
+    TasksModel.updateOne(query, status).then((document) => {
+        res.status(200).json({success : true, data : document});
+    }).catch((error) => {
+        res.status(400).json({success : false, data : error});
+    })
+}
+
--- src/route/api.js
+++ src/route/api.js
@@ -7,12 +7,15 @@ const AuthVerifyMiddleware = require("../middleware/AuthVerifyMiddleware")
 
 // Import Controller
 const UsersController = require("../controller/UsersController")
+const TasksController = require("../controller/TasksController")
 
 
 // Route Path
 router.post("/registration", UsersController.registration);
 router.post("/login", UsersController.login);
 router.post("/profile-update", AuthVerifyMiddleware, UsersController.profileUpdate);
+
+router.post("/create-task", AuthVerifyMiddleware, TasksController.createTask);
 
 
 module.exports=router;
