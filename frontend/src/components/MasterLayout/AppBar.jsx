9dee51bc079d6ee31a839be3852dad37acb83d34
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-03 01:44:36 +0600

    Add AppBar top-right User Profile Dropdown menu

--- frontend/src/components/MasterLayout/AppBar.jsx
+++ frontend/src/components/MasterLayout/AppBar.jsx
@@ -6,11 +6,20 @@ import Typography from "@mui/material/Typography";
 import Badge from "@mui/material/Badge";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import {StyledAppBar} from "../../styeldComponent/DashboardLayout.js";
+import Box from "@mui/material/Box";
+import {Avatar, Menu, MenuItem, Tooltip} from "@mui/material";
 
 const AppBarComponent = ({open, clickDrawer}) => {
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
     const toggleDrawer = () => {
         clickDrawer()
     }
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
     return (
         <StyledAppBar position="absolute" open={open}>
@@ -40,11 +49,36 @@ const AppBarComponent = ({open, clickDrawer}) => {
                 >
                     Dashboard
                 </Typography>
-                <IconButton color="inherit">
-                    <Badge badgeContent={4} color="secondary">
-                        <NotificationsIcon/>
-                    </Badge>
-                </IconButton>
+                <Box sx={{ flexGrow: 0 }}>
+                    <Tooltip title="Open settings">
+                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        sx={{ mt: '45px' }}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        <MenuItem  onClick={handleCloseUserMenu}>
+                            <Typography textAlign="center">Profile</Typography>
+                        </MenuItem>
+                        <MenuItem  onClick={handleCloseUserMenu}>
+                            <Typography textAlign="center">Logout</Typography>
+                        </MenuItem>
+                    </Menu>
+                </Box>
             </Toolbar>
         </StyledAppBar>
     );
