243174fca4a686c375bb035656964f5ba100c832
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-05 05:40:55 +0600

    fix issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
     host: true,
     port: 3000, // This is the port which we will use in docker
   }
