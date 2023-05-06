4b22e6fd0c3a06b3fd01c452a77c02d90febbe2e
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-07 03:54:53 +0600

    Add register form validation

--- frontend/src/components/Registration/Registration.jsx
+++ frontend/src/components/Registration/Registration.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import {Avatar, Checkbox, FormControlLabel, TextField} from "@mui/material";
+import React, {useRef} from 'react';
+import {TextField} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import {Link} from "react-router-dom";
+import {ErrorToast, IsEmail, IsEmpty, IsMobile} from "../../helper/FormHelper.js";
 
 const Registration = () => {
+
+    const emailRef = useRef(null);
+    const firstNameRef = useRef(null);
+    const lastNameRef = useRef(null);
+    const mobileRef = useRef(null);
+    const passwordRef = useRef(null);
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
+
+        let email = emailRef.current.value;
+        let firstName = firstNameRef.current.value;
+        let lastName = lastNameRef.current.value;
+        let mobile = mobileRef.current.value;
+        let password = passwordRef.current.value;
+
+        if(IsEmail(email)){
+            ErrorToast("Valid Email Address Required !")
+        }
+        else if(IsEmpty(firstName)){
+            ErrorToast("First Name Required !")
+        }
+        else if(IsEmpty(lastName)){
+            ErrorToast("Last Name Required !")
+        }
+        else if(!IsMobile(mobile)){
+            ErrorToast("Valid Mobile  Required !")
+        }
+        else if(IsEmpty(password)){
+            ErrorToast("Password Required !")
+        }
+        else{
+            // RegistrationRequest(email,fastName,lastName,mobile,password,photo).then((result)=>{
+            //     if(result===true){
+            //         navigate("/login")
+            //     }
+            // })
+        }
+
     };
     return (
         <>
@@ -27,42 +60,43 @@ const Registration = () => {
                     <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
                         <TextField
                             margin="normal"
-                            required
                             fullWidth
                             label="Email Address"
                             type="email"
+                            inputRef={emailRef}
                         />
                         <TextField
                             margin="normal"
-                            required
                             fullWidth
                             label="First Name"
+                            inputRef={firstNameRef}
                         />
                         <TextField
                             margin="normal"
-                            required
                             fullWidth
                             label="Last Name"
+                            inputRef={lastNameRef}
                         />
                         <TextField
                             margin="normal"
-                            required
                             fullWidth
                             label="Mobile"
                             type="number"
+                            inputRef={mobileRef}
                         />
                         <TextField
                             margin="normal"
-                            required
                             fullWidth
                             label="Password"
                             type="password"
+                            inputRef={passwordRef}
                         />
                         <Button
                             type="submit"
                             fullWidth
                             variant="contained"
                             sx={{mt: 3, mb: 2}}
+                            onClick={handleSubmit}
                         >
                             Sign In
                         </Button>
