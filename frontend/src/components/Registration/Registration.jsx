62b1cb380722eea5d66263723e99949e0b799dfe
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-07 02:11:38 +0600

    Made Registration Component UI

--- frontend/src/components/Registration/Registration.jsx
+++ frontend/src/components/Registration/Registration.jsx
@@ -1,10 +1,87 @@
 import React from 'react';
+import {Avatar, Checkbox, FormControlLabel, TextField} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import {Link} from "react-router-dom";
 
 const Registration = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
     return (
-        <div>
-            Registration
-        </div>
+        <>
+            <Grid  container direction="row" justifyContent="center" alignItems="center" sx={{paddingTop : '50px'}} >
+                <Grid item xs={10} md={3}>
+                    <Box sx={{textAlign : 'center'}}>
+                        <Typography component="h1" variant="h4">
+                            Sign Up
+                        </Typography>
+                    </Box>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Email Address"
+                            type="email"
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="First Name"
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Last Name"
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Mobile"
+                            type="number"
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Password"
+                            type="password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link to="/forget-password" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link to="/login" variant="body2">
+                                    {"Already have an account? Sign In"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Grid>
+            </Grid >
+        </>
     );
 };
 
