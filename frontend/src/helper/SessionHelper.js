5ed955d853b6c82c922d542d985448c02db9c0a5
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-07 18:35:52 +0600

    Made session helper file, class and methods to store login user info and get info

---
+++ frontend/src/helper/SessionHelper.js
@@ -0,0 +1,16 @@
+class SessionHelper{
+    setToken(token){
+        localStorage.setItem("token", token);
+    }
+    getToken(){
+       return  localStorage.getItem("token");
+    }
+    setUserDetails(data){
+        localStorage.setItem("userDetails", JSON.stringify(data));
+    }
+    getUserDetails(){
+       return  JSON.parse(localStorage.getItem("userDetails"));
+    }
+}
+
+export const {setToken, getToken, setUserDetails, getUserDetails} = new  SessionHelper();
