24b2a6336ab66ef969d9f0a8d62caed21be38b3f
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-07 18:35:18 +0600

    Remove strict mode and  import roboto font

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,17 @@ import App from './App.jsx'
 import './assets/css/global.css'
 import {ThemeProvider} from "@mui/material";
 import {theme} from "./theme"
+import {Provider} from "react-redux";
+import store from "./redux/store/store.js";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-  </React.StrictMode>,
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </Provider>
 )
