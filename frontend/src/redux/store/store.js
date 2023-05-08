e286882df13fc8b860f46614f72f5e9f82203a38
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-09 03:52:45 +0600

    connect portfolio slice in store

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,11 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit"
-import settingsReducer from '../state-slice/Settings-slice';
+import settingsReducer from '../state-slice/SettingsSlice.js';
 import taskReducer from '../state-slice/TaskSlice';
 import summaryReducer from '../state-slice/SummarySlice';
+import profileReducer from "../state-slice/ProfileSlice";
+
 export default configureStore({
     reducer : {
         settings : settingsReducer,
         task : taskReducer,
         summary : summaryReducer,
+        profile : profileReducer
     }
 })
