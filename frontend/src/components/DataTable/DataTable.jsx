47fe165b4993234cbd436bbe93e2c54f2a732a2a
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-14 23:14:55 +0600

    Made data table component and table functionality added

---
+++ frontend/src/components/DataTable/DataTable.jsx
@@ -0,0 +1,277 @@
+import React, {useEffect, useState} from "react";
+import PropTypes from 'prop-types';
+import {alpha} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import {getTaskList} from "../../APIRequest/APIRequest.js";
+import {useSelector} from "react-redux";
+import {Search, SearchIconWrapper, StyledInputBase} from "../../styeldComponent/SearchField.js";
+import SearchIcon from "@mui/icons-material/Search.js";
+
+
+const headCells = [
+    {
+        id: 'title',
+        numeric: false,
+        disablePadding: true,
+        label: 'Title',
+    },
+    {
+        id: 'status',
+        numeric: true,
+        disablePadding: false,
+        label: 'Status',
+    },
+    {
+        id: 'createdDate',
+        numeric: true,
+        disablePadding: false,
+        label: 'Created Date',
+    },
+    {
+        id: 'action',
+        numeric: false,
+        disablePadding: false,
+        label: 'Action',
+    }
+];
+
+function EnhancedTableHead(props) {
+    const {onSelectAllClick, numSelected, rowCount} = props;
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        color="primary"
+                        indeterminate={numSelected > 0 && numSelected < rowCount}
+                        checked={rowCount > 0 && numSelected === rowCount}
+                        onChange={onSelectAllClick}
+                        inputProps={{
+                            'aria-label': 'select all desserts',
+                        }}
+                    />
+                </TableCell>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                    >
+                        {headCell.label}
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
+
+function EnhancedTableToolbar(props) {
+    const {numSelected} = props;
+
+    return (
+        <Toolbar
+            sx={{
+                pl: {sm: 2},
+                pr: {xs: 2, sm: 2},
+                ...(numSelected > 0 && {
+                    bgcolor: (theme) =>
+                        alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+                }),
+            }}
+        >
+            {numSelected > 0 ? (
+                <Typography
+                    sx={{flex: '1 1 100%'}}
+                    color="inherit"
+                    variant="subtitle1"
+                    component="div"
+                >
+                    {numSelected} selected
+                </Typography>
+            ) : (
+                <Typography
+                    sx={{flex: '1 1 100%'}}
+                    variant="h6"
+                    id="tableTitle"
+                    component="div"
+                >
+                    Task List
+                </Typography>
+            )}
+
+            {numSelected > 0 ? (
+                <Tooltip title="Delete">
+                    <IconButton>
+                        <DeleteIcon/>
+                    </IconButton>
+                </Tooltip>
+            ) : (
+                <div>
+                    <Search sx={{width : '200px'}}>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
+                </div>
+            )}
+        </Toolbar>
+    );
+}
+
+EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+};
+
+export default function DataTable() {
+
+    const [selected, setSelected] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+
+    let [searchKey, setSearchKey] = useState("0");
+    let [perPageKey, setPerPageKey] = useState(5);
+
+    useEffect(() => {
+        getTaskList(1, perPageKey, searchKey)
+    }, [])
+
+    let ALLTask = useSelector((state) => (state.task.ALLTask));
+
+    console.log("ALLTask : ", ALLTask);
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelected = ALLTask.map((n) => n._id);
+            setSelected(newSelected);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleClick = (event, name) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1),
+            );
+        }
+
+        setSelected(newSelected);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const isSelected = (name) => selected.indexOf(name) !== -1;
+
+    return (
+        <Box sx={{width: '100%'}}>
+            <Paper sx={{width: '100%', mb: 2}}>
+                <EnhancedTableToolbar numSelected={selected.length}/>
+                <TableContainer>
+                    <Table
+                        sx={{minWidth: 750}}
+                        aria-labelledby="tableTitle"
+                    >
+                        <EnhancedTableHead
+                            numSelected={selected.length}
+                            onSelectAllClick={handleSelectAllClick}
+                            rowCount={ALLTask.length}
+                        />
+                        <TableBody>
+                            {ALLTask.map((row, index) => {
+                                const isItemSelected = isSelected(row._id);
+                                const labelId = `enhanced-table-checkbox-${index}`;
+                                return (
+                                    <TableRow
+                                        hover
+                                        onClick={(event) => handleClick(event, row._id)}
+                                        role="checkbox"
+                                        aria-checked={isItemSelected}
+                                        tabIndex={-1}
+                                        key={row.title}
+                                        selected={isItemSelected}
+                                        sx={{cursor: 'pointer'}}
+                                    >
+                                        <TableCell padding="checkbox">
+                                            <Checkbox
+                                                color="primary"
+                                                checked={isItemSelected}
+                                                inputProps={{
+                                                    'aria-labelledby': labelId,
+                                                }}
+                                            />
+                                        </TableCell>
+                                        <TableCell
+                                            component="th"
+                                            id={labelId}
+                                            scope="row"
+                                            padding="none"
+                                        >
+                                            {row.title}
+                                        </TableCell>
+                                        <TableCell align="right">{row.status}</TableCell>
+                                        <TableCell align="right">{row.createdDate}</TableCell>
+                                        <TableCell align="right">
+
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={ALLTask.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </Box>
+    );
+}
