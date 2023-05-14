29872a675b4d48f5546f87775617fb229f4a77ba
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-14 23:12:33 +0600

    Made API Controller for task List

--- api/src/controller/TasksController.js
+++ api/src/controller/TasksController.js
@@ -74,3 +74,24 @@ exports.taskStatusCount = (req, res) => {
         res.status(400).json({success: false, data: error});
     })
 }
+
+
+exports.TaskList=async (req, res) => {
+    let pageNo = Number(req.params.pageNo);
+    let perPage = Number(req.params.perPage);
+    let searchValue = req.params.searchKey;
+    const skipRow = (pageNo - 1) * perPage;
+    let Rows;
+    let Total;
+    if (searchValue!=="0") {
+        let SearchRgx = {"$regex": searchValue, "$options": "i"}
+        let SearchQuery = {$or: [{title: SearchRgx}]}
+
+        Total = (await TasksModel.aggregate([{$match: SearchQuery}, {$count: "total"}]))[0]['total']
+        Rows = await TasksModel.aggregate([{$match: SearchQuery}, {$skip: skipRow}, {$limit: perPage}])
+    } else {
+        Total = (await TasksModel.aggregate([{$count: "total"}]))[0]['total']
+        Rows = await TasksModel.aggregate([{$skip: skipRow}, {$limit: perPage}])
+    }
+    res.status(200).json({status: "success", total: Total, data: Rows})
+}
