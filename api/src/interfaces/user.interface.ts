6a31458707d9ecba372a2e823656443717d91c89
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-17 04:08:08 +0600

    update user interface

--- api/src/interfaces/user.interface.ts
+++ api/src/interfaces/user.interface.ts
@@ -2,7 +2,7 @@ import { Document } from 'mongoose';
 
 export interface UserInterface extends Document {
     email: string;
-    firstName: String;
+    firstName: string;
     lastName: string;
     mobile: string;
     password: string;
