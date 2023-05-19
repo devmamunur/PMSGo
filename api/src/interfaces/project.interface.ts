aea6f2ff802a0bc5cce51b89a04021ea4c9106c8
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-20 01:03:55 +0600

    create project interface

---
+++ api/src/interfaces/project.interface.ts
@@ -0,0 +1,12 @@
+import { Document } from 'mongoose';
+export interface ProjectInterface extends Document {
+    title: string;
+    description: string;
+    dueDate?: Date;
+    startDate?: Date;
+    endDate?: Date;
+    createdDate: Date;
+    organization: Types.ObjectId;
+    assignedUsers: Types.ObjectId[];
+    tasks: Types.ObjectId[];
+}
