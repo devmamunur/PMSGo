83eff91b1ba7df8ad0ffb2562faf65f39d4585e6
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-22 03:20:56 +0600

    fix imports

--- frontend/src/components/Completed/Completed.tsx
+++ frontend/src/components/Completed/Completed.tsx
@@ -1,13 +1,13 @@
 import React, {useEffect} from 'react';
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import {Search, SearchIconWrapper, StyledInputBase} from "../../styeldComponent/SearchField.js";
+import {Search, SearchIconWrapper, StyledInputBase} from "@/styeldComponent/SearchField";
 import SearchIcon from "@mui/icons-material/Search";
-import {taskListByStatus} from "../../APIRequest/APIRequest.js";
+import {taskListByStatus} from "@/APIRequest/APIRequest";
 import {useSelector} from "react-redux";
-import {DeleteToDO} from "../../helper/DeleteAlert.js";
-import {UpdateTask} from "../../helper/UpdateAlert.js";
-import TaskCard from "../TaskCard/TaskCard.jsx";
+import {DeleteToDO} from "@/helper/DeleteAlert";
+import {UpdateTask} from "@/helper/UpdateAlert";
+import TaskCard from "@/components/TaskCard/TaskCard";
 
 const Completed = () => {
     const taskCompleted = useSelector((state) => state.task.Completed);
