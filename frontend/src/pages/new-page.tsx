e84ae67f9ab8b20fb364cd0dc383e6936904b0e6
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-22 03:22:39 +0600

    fix imports

--- frontend/src/pages/new-page.tsx
+++ frontend/src/pages/new-page.tsx
@@ -1,8 +1,8 @@
 import React, {lazy, Suspense} from 'react';
-import MasterLayout from "../app/components/MasterLayout/MasterLayout.jsx";
-import LazyLoader from "../app/components/MasterLayout/LazyLoader.jsx";
+import MasterLayout from "@/components/MasterLayout/MasterLayout";
+import LazyLoader from "@/components/MasterLayout/LazyLoader";
 
-const New = lazy(() => import('../app/components/New/New.jsx'));
+const New = lazy(() => import('@/components/New/New'));
 const NewPage = () => {
 
     return (
