0a91e51e70c19803d9cc262e36cb82be3888dbd3
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-22 04:01:14 +0600

    make task interface  file and add interface

---
+++ frontend/src/interfaces/task.interface.ts
@@ -0,0 +1,13 @@
+export interface Task {
+    _id: string;
+    title: string;
+    description: string;
+    createdDate: string;
+    status: string;
+}
+
+export interface TaskCardProps {
+    task: Task;
+    UpdateItem: (id: string, status: string) => void;
+    DeleteItem: (id: string) => void;
+}
