6b1a893668e7d76e9a18991e9d02e48a858f5d30
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-22 03:22:58 +0600

    fix imports

--- frontend/src/pages/create-page.tsx
+++ frontend/src/pages/create-page.tsx
@@ -1,8 +1,8 @@
 import React, {lazy, Suspense} from 'react';
-import MasterLayout from "../app/components/MasterLayout/MasterLayout.jsx";
-import LazyLoader from "../app/components/MasterLayout/LazyLoader.jsx";
+import MasterLayout from "@/components/MasterLayout/MasterLayout";
+import LazyLoader from "@/components/MasterLayout/LazyLoader";
 
-const Create = lazy(() => import('../app/components/Create/Create.jsx'));
+const Create = lazy(() => import('@/components/Create/Create'));
 const CreatePage = () => {
     return (
         <>
