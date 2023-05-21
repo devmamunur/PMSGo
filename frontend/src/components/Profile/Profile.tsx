83fc6f59b884cebac02207f16015c85f6895cac6
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-22 04:04:25 +0600

    Update profile component in typescript

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef} from 'react';
-import {GetProfileDetails, ProfileUpdateRequest} from "../../APIRequest/APIRequest.js";
+"use client"
+import React, {useEffect, useRef, useState} from 'react';
 import {useSelector} from "react-redux";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
@@ -7,59 +7,58 @@ import CardContent from "@mui/material/CardContent";
 import {Avatar, Box, CardHeader, TextField} from "@mui/material";
 import Divider from "@mui/material/Divider";
 import Button from "@mui/material/Button";
-import {ErrorToast, getBase64, IsEmail, IsEmpty, IsMobile} from "../../helper/FormHelper.js";
-import {useNavigate} from "react-router-dom";
+import UserRequest from "@/APIRequests/user.request";
+import FormHelper from "@/helpers/form.helper";
+import ToastHelper from "@/helpers/toast.helper";
+import {redirect} from "next/navigation";
+import {RootState} from "@/redux/store/store";
 
-const Profile = () => {
-    let navigate=useNavigate();
+const Profile: React.FC = () => {
 
-    const emailRef = useRef(null);
-    const firstNameRef = useRef(null);
-    const lastNameRef = useRef(null);
-    const mobileRef = useRef(null);
-    const passwordRef = useRef(null);
-    const photoRef = useRef(null);
-    let userImgView = useRef(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const firstNameRef = useRef<HTMLInputElement>(null);
+    const lastNameRef = useRef<HTMLInputElement>(null);
+    const mobileRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const photoRef = useRef<HTMLInputElement>(null);
+    const [photo, setPhoto] = useState<any>();
 
     useEffect(() => {
-        GetProfileDetails()
+        UserRequest.getProfileDetails()
     }, [])
 
-    const ProfileData = useSelector((state) => state.profile.value);
+    const ProfileData = useSelector((state: RootState) => state.profile.value);
 
     const PreviewImage = () => {
-        let ImgFile = photoRef.current.files[0];
-        getBase64(ImgFile).then((base64Img)=>{
-            userImgView.src=base64Img;
-        })
+        if (photoRef.current && photoRef.current.files && photoRef.current.files.length > 0) {
+            let ImgFile = photoRef.current.files[0];
+            FormHelper.getBase64(ImgFile).then((base64Img: any) => {
+                setPhoto(base64Img);
+            })
+        }
     }
 
 
-    const handleSubmit = (event) => {
+    const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
-        let email = emailRef.current.value;
-        let firstName = firstNameRef.current.value;
-        let lastName = lastNameRef.current.value;
-        let mobile = mobileRef.current.value;
-        let password = passwordRef.current.value;
-        let photo = userImgView.src;
+        let email = emailRef.current!.value;
+        let firstName = firstNameRef.current!.value;
+        let lastName = lastNameRef.current!.value;
+        let mobile = mobileRef.current!.value;
+        let password = passwordRef.current!.value;
 
-        if(IsEmail(email)){
-            ErrorToast("Valid Email Address Required !")
-        }
-        else if(IsEmpty(firstName)){
-            ErrorToast("First Name Required !")
-        }
-        else if(IsEmpty(lastName)){
-            ErrorToast("Last Name Required !")
-        }
-        else if(!IsMobile(mobile)){
-            ErrorToast("Valid Mobile  Required !")
-        }
-        else{
-            ProfileUpdateRequest(email,firstName,lastName,mobile,password,photo).then((result)=>{
-                if(result===true){
-                    navigate("/")
+        if (FormHelper.isEmail(email)) {
+            ToastHelper.errorToast("Valid Email Address Required !")
+        } else if (FormHelper.isEmpty(firstName)) {
+            ToastHelper.errorToast("First Name Required !")
+        } else if (FormHelper.isEmpty(lastName)) {
+            ToastHelper.errorToast("Last Name Required !")
+        } else if (!FormHelper.isMobile(mobile)) {
+            ToastHelper.errorToast("Valid Mobile  Required !")
+        } else {
+            UserRequest.profileUpdateRequest(email, firstName, lastName, mobile, password, photo).then((result: boolean) => {
+                if (result) {
+                    redirect("/")
                 }
             })
         }
@@ -68,23 +67,23 @@ const Profile = () => {
         <>
             {ProfileData ?
                 (
-                    <Grid container justifyContent="center" spacing={3} >
+                    <Grid container justifyContent="center" spacing={3}>
                         <Grid item xs={12} md={6}>
                             <Card>
-                                <CardHeader sx={{textAlign : 'center'}} title="Update Profile">
+                                <CardHeader sx={{textAlign: 'center'}} title="Update Profile">
                                 </CardHeader>
-                                <CardContent  component="form" onSubmit={handleSubmit} sx={{textAlign : 'center'}}>
-                                    <Box sx={{textAlign : 'center', width : '100%'}}>
+                                <CardContent component="form" onSubmit={handleSubmit} sx={{textAlign: 'center'}}>
+                                    <Box sx={{textAlign: 'center', width: '100%'}}>
                                         <Avatar
                                             alt="Remy Sharp"
                                             src={ProfileData.photo}
-                                            sx={{ width: 100, height: 100, display : 'inline-block'}}
+                                            sx={{width: 100, height: 100, display: 'inline-block'}}
                                         />
                                     </Box>
                                     <br/>
                                     <Divider/>
                                     <br/>
-                                    <Grid container  spacing={2} >
+                                    <Grid container spacing={2}>
                                         <Grid item xs={12} md={6}>
                                             <TextField
                                                 required
@@ -146,8 +145,9 @@ const Profile = () => {
                                                 defaultValue=""
                                             />
                                         </Grid>
-                                        <Grid item xs={12}  sx={{textAlign : 'center', marginTop : '15px'}}>
-                                            <Button sx={{display : 'inline-block'}} onClick={handleSubmit} variant="contained" color="primary">Update</Button>
+                                        <Grid item xs={12} sx={{textAlign: 'center', marginTop: '15px'}}>
+                                            <Button sx={{display: 'inline-block'}} onClick={handleSubmit}
+                                                    variant="contained" color="primary">Update</Button>
                                         </Grid>
                                     </Grid>
                                 </CardContent>
