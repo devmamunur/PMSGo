dc3bee79207764d6d0d8839f009239da4b06146c
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-22 03:59:56 +0600

    Update profile slice in typescript

--- frontend/src/redux/state-slice/ProfileSlice.ts
+++ frontend/src/redux/state-slice/ProfileSlice.ts
@@ -1,14 +1,20 @@
-import {createSlice} from "@reduxjs/toolkit";
-export const profileSlice=createSlice({
-    name:'profile',
-    initialState:{
-        value:[]
-    },
-    reducers:{
-        SetProfile:(state,action)=>{
-            state.value=action.payload
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface ProfileState {
+    value: any
+}
+
+const initialState: ProfileState = {
+    value: []
+}
+export const profileSlice = createSlice({
+    name: 'profile',
+    initialState,
+    reducers: {
+        setProfile: (state, action: PayloadAction<any[]>) => {
+            state.value = action.payload;
         }
     }
 })
-export  const {SetProfile}=profileSlice.actions;
-export default  profileSlice.reducer;
+export const {setProfile} = profileSlice.actions;
+export default profileSlice.reducer;
