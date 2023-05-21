da374fdfea78b2f5bde1260d934b8d64ef5d732f
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-22 04:00:21 +0600

    Update summary slice in typescript

--- frontend/src/redux/state-slice/SummarySlice.ts
+++ frontend/src/redux/state-slice/SummarySlice.ts
@@ -1,14 +1,23 @@
-import {createSlice} from "@reduxjs/toolkit";
-export const summarySlice=createSlice({
-    name:'summary',
-    initialState:{
-        value:[]
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface SummaryState {
+    value: any;
+}
+
+const initialState: SummaryState = {
+    value: [],
+};
+
+export const summarySlice = createSlice({
+    name: 'summary',
+    initialState,
+    reducers: {
+        setSummary: (state, action: PayloadAction<any[]>) => {
+            state.value = action.payload;
+        },
     },
-    reducers:{
-        SetSummary:(state,action)=>{
-            state.value=action.payload
-        }
-    }
-})
-export  const {SetSummary}=summarySlice.actions;
-export default  summarySlice.reducer;
+});
+
+export const { setSummary } = summarySlice.actions;
+
+export default summarySlice.reducer;
