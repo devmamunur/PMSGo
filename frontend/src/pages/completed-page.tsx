137601b185cd3fca9cf17a7f26b7fb40b8bacd2c
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-22 03:23:03 +0600

    fix imports

--- frontend/src/pages/completed-page.tsx
+++ frontend/src/pages/completed-page.tsx
@@ -1,8 +1,8 @@
 import React, {lazy, Suspense} from 'react';
-import MasterLayout from "../app/components/MasterLayout/MasterLayout.jsx";
-import LazyLoader from "../app/components/MasterLayout/LazyLoader.jsx";
+import MasterLayout from "@/components/MasterLayout/MasterLayout";
+import LazyLoader from "@/components/MasterLayout/LazyLoader";
 
-const Completed = lazy(() => import('../app/components/Completed/Completed.jsx'));
+const Completed = lazy(() => import('@/components/Completed/Completed'));
 const CompletedPage = () => {
     return (
         <>
