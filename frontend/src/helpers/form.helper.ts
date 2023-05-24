e1c3591071511b59a1d41b7a79322e8a1d045f05
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-24 10:01:49 +0600

    update mobile number validation

--- frontend/src/helpers/form.helper.ts
+++ frontend/src/helpers/form.helper.ts
@@ -1,13 +1,12 @@
 import { toast  } from 'react-toastify';
 let EmailRegx = /\S+@\S+\.\S+/;
-let MobileRegx = /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/;
 
 class FormHelper {
     static isEmpty(value : any) : boolean {
         return value.length === 0;
     }
     static isMobile(value : any): boolean{
-        return MobileRegx.test(value);
+        return value > 9;
     }
     static isEmail(value : any): boolean {
         return !EmailRegx.test(value);
