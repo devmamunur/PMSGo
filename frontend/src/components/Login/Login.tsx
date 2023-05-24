e6f34aaa6072129b34b48fec63cf91f1d6e3da29
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-25 02:46:06 +0600

    made login page

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,35 +1,33 @@
+"use client"
 import React, {useRef} from 'react';
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import {TextField} from "@mui/material";
+import {Card, CardContent, TextField} from "@mui/material";
 import Button from "@mui/material/Button";
 import FormHelper from "@/helpers/form.helper";
 import ToastHelper from "@/helpers/toast.helper";
 import UserRequest from "@/APIRequests/user.request";
 import Link from "next/link";
 
-const Login : React.FC = () => {
-
-
+const Login: React.FC = () => {
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
 
-    const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         let email = emailRef.current!.value;
         let password = passwordRef.current!.value;
 
-        if(FormHelper.isEmail(email)){
+        if (FormHelper.isEmail(email)) {
             ToastHelper.errorToast("Valid Email Address Required !")
-        }
-        else if(FormHelper.isEmpty(password)){
+        } else if (FormHelper.isEmpty(password)) {
             ToastHelper.errorToast("Password Required !")
-        }else {
-            UserRequest.loginRequest(email, password).then((result : boolean) => {
-                if(result) {
-                    window.location.href="/"
+        } else {
+            UserRequest.loginRequest(email, password).then((result: boolean) => {
+                if (result) {
+                    window.location.href = "/"
                 }
             });
         }
@@ -37,53 +35,65 @@ const Login : React.FC = () => {
 
     return (
         <>
-            <Grid  container direction="row" justifyContent="center" alignItems="center" sx={{paddingTop : '50px'}} >
-                <Grid item xs={10} md={3}>
-                    <Box sx={{textAlign : 'center'}}>
-                        <Typography component="h1" variant="h4">
-                            Sign In
-                        </Typography>
-                    </Box>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Email Address"
-                            type="email"
-                            inputRef={emailRef}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Password"
-                            type="password"
-                            inputRef={passwordRef}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 3, mb: 2}}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="/send-otp" >
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="/register">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
+            <Grid container justifyContent="center" alignItems="center" sx={{
+                backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                        ? theme.palette.grey[100]
+                        : theme.palette.grey[900],
+                flexGrow: 1,
+                height: '100vh',
+                overflow: 'auto',
+            }}>
+                <Grid item xs={10} xl={4}>
+                    <Card>
+                        <CardContent>
+                            <Box sx={{textAlign: 'center'}}>
+                                <Typography component="h1" variant="h4">
+                                    Sign In
+                                </Typography>
+                            </Box>
+                            <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    label="Email Address"
+                                    type="email"
+                                    inputRef={emailRef}
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    label="Password"
+                                    type="password"
+                                    inputRef={passwordRef}
+                                />
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{mt: 3, mb: 2}}
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container>
+                                    <Grid item xs>
+                                        <Link href="/send-otp">
+                                            Forgot password?
+                                        </Link>
+                                    </Grid>
+                                    <Grid item>
+                                        <Link href="/register">
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </CardContent>
+                    </Card>
                 </Grid>
-            </Grid >
+            </Grid>
         </>
     );
 };
