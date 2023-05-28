597eae2272a733f4f7079482aae63aadd8fc69b1
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-29 03:25:29 +0600

    Make RootLayoutProps  interface

---
+++ frontend/src/interfaces/layout-props.interface.ts
@@ -0,0 +1,10 @@
+import React from "react";
+import {Session} from "next-auth";
+export interface RootLayoutProps {
+    children: React.ReactNode;
+}
+
+export interface RootLayoutPropsWithSession {
+    children: React.ReactNode;
+    session: Session | null;
+}
