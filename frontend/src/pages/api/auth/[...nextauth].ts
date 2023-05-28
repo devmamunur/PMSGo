63f6c4f88642b46872aefa60541ebbe30d12082d
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-29 03:23:04 +0600

    update next auth  code

---
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,56 @@
+import NextAuth, {NextAuthOptions } from 'next-auth';
+import CredentialProvider from 'next-auth/providers/credentials';
+
+export const authOptions : NextAuthOptions  = {
+    providers: [
+        CredentialProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: 'Email', type: 'email', placeholder: 'email@example.com' },
+                password: { label: 'Password', type: 'password' },
+            },
+            authorize: async (credentials: Record<string, string> | undefined) => {
+                try {
+                    let payload = {
+                        email   : credentials?.email,
+                        password: credentials?.password
+                    }
+
+                    const response = await fetch(process.env.NEXT_PUBLIC_API_URL+'/login', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(payload),
+                    });
+                    const user = await response.json();
+                    if (user) {
+                        return user;
+                    } else {
+                        return null;
+                    }
+                } catch (error) {
+                    console.error('Failed to authenticate:', error);
+                    return null;
+                }
+            },
+        }),
+    ],
+    session  : {
+        strategy: "jwt"
+    },
+    pages    : {
+        signIn: '/login'
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            return { ...token, ...user };
+        },
+        async session({ session, token, user }) {
+            session.user = token as any;
+            return session;
+        },
+
+    },
+}
+export default NextAuth(authOptions)
