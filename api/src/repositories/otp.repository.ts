abe1da924f60264ffe60917387ac0c6b2281a2e1
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-30 04:38:14 +0600

    update and modify otp repository

--- api/src/repositories/otp.repository.ts
+++ api/src/repositories/otp.repository.ts
@@ -1,59 +1,65 @@
 import OTPModel from "../models/otp.model";
 import SendEmailUtility from "../utility/send-email.utility";
 import UserModel from "../models/user.model";
+import {Request} from "express";
+import GeneratePasswordUtility from "../utility/generate-password.utility";
 
 class OtpRepository {
-    static async RecoverVerifyEmail(email: any) {
+    static async RecoverVerifyEmail(reqBody: Request['body']) {
         let otpCode = Math.floor(100000 + Math.random() * 900000);
-        let UserCount = (await UserModel.aggregate([{$match: {email: email}}, {$count: "total"}]))
+        let UserCount = (await UserModel.aggregate([{$match: {email: reqBody.email}}, {$count: "total"}]))
         if (UserCount.length > 0) {
-            let CreateOTP = await OTPModel.create({email: email, otp: otpCode})
-            let SendEmail = await SendEmailUtility(email, "Your PIN Code is= " + otpCode, "Task Manager PIN Verification")
+            let CreateOTP = await OTPModel.create({email: reqBody.email, otp: otpCode})
+            let SendEmail = await SendEmailUtility(reqBody.email, "Your PIN Code is = " + otpCode, "Task Manager PIN Verification")
             return {SendEmail};
         } else {
             throw new Error("No User Found");
         }
     }
 
-    static async RecoverVerifyOTP(email: any, otpCode: any) {
-        let status = 0;
-        let statusUpdate = 1;
-        let otpCount = await OTPModel.aggregate([{
-            $match: {
-                email: email,
-                otp: otpCode,
-                status: status
-            }
-        }, {$count: "total"}]);
+    static async RecoverVerifyOTP(reqBody : Request['body']) {
+        try{
+            let status = 0;
+            let statusUpdate = 1;
+            let otpCount = await OTPModel.aggregate([{
+                $match: {
+                    email: reqBody.email,
+                    otp: reqBody.OTP,
+                    status: status
+                }
+            }, {$count: "total"}]);
 
-        if (otpCount.length > 0) {
-            let otpUpdate = await OTPModel.updateOne({email: email, otp: otpCode, status: status}, {
-                email: email,
-                otp: otpCode,
-                status: statusUpdate
-            })
-            return {otpUpdate};
-        } else {
-            throw new Error("Invalid OTP Code");
+            if (otpCount.length > 0) {
+                let otpUpdate = await OTPModel.updateOne({email: reqBody.email, otp: reqBody.OTP, status: status}, {
+                    email:  reqBody.email,
+                    otp: reqBody.OTP,
+                    status: statusUpdate
+                })
+                return {otpUpdate};
+            }
+        }catch (error){
+            throw error
         }
     }
 
-    static async RecoverResetPass(email : any, otpCode : any, newPass : any){
-        let statusUpdate = 1;
-        let otpUsedCount = await OTPModel.aggregate([{
-            $match: {
-                email: email,
-                otp: otpCode,
-                status: statusUpdate
+    static async RecoverResetPass(reqBody : Request['body']){
+        try{
+            let statusUpdate = 1;
+            let otpUsedCount = await OTPModel.aggregate([{
+                $match: {
+                    email: reqBody.email,
+                    otp: reqBody.OTP,
+                    status: statusUpdate
+                }
+            }, {$count: "total"}])
+            if (otpUsedCount.length > 0) {
+                let passUpdate = await UserModel.updateOne({email: reqBody.email}, {
+                    password:  GeneratePasswordUtility(reqBody.password)
+                })
+                return {passUpdate};
             }
-        }, {$count: "total"}])
-        if (otpUsedCount.length > 0) {
-            let passUpdate = await UserModel.updateOne({email: email}, {
-                password: newPass
-            })
-            return {passUpdate};
-        } else {
-            throw new Error("Invalid Request");
+        } catch (error){
+            throw error
         }
     }
 }
