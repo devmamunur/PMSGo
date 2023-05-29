89944227bb163129084be97a50bdb01630fa685e
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-30 04:35:28 +0600

    made create password page

---
+++ frontend/src/app/(auth)/create-password/page.tsx
@@ -0,0 +1,15 @@
+import React, {lazy, Suspense} from 'react';
+import LazyLoader from "@/components/MasterLayout/LazyLoader";
+
+const CreatePassword = lazy(() => import('@/components/PasswordRecover/CreatePassword'));
+const CreatePasswordPage = () => {
+    return (
+        <>
+            <Suspense fallback={<LazyLoader/>}>
+                <CreatePassword/>
+            </Suspense>
+        </>
+    );
+};
+
+export default CreatePasswordPage;
