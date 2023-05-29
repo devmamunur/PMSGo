dc0fbfe86d89d8aa16bc72d58e56f23050f69bbb
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-30 04:33:23 +0600

    modify and update CreatePassword component

--- frontend/src/components/PasswordRecover/CreatePassword.tsx
+++ frontend/src/components/PasswordRecover/CreatePassword.tsx
@@ -1,41 +1,42 @@
 "use client"
-import React, {useRef} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import {TextField} from "@mui/material";
-import {redirect} from "next/navigation";
+import {Card, CardContent, TextField} from "@mui/material";
+import {useRouter} from "next/navigation";
 import UserRequest from "@/APIRequests/user.request";
 import SessionHelper from "@/helpers/session.helper";
 import ToastHelper from "@/helpers/toast.helper";
 import FormHelper from "@/helpers/form.helper";
+import { LoadingButton } from '@mui/lab';
+import {Send} from "@mui/icons-material";
 
 const CreatePassword: React.FC = () => {
+    const router = useRouter();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [password, setPassword] = useState<string>("");
+    const [confirmPassword, setConfirmPassword] = useState<string>("");
 
-    const PasswordRef = useRef<HTMLInputElement>(null);
-    const ConfirmPasswordRef = useRef<HTMLInputElement>(null);
-
-    const handleSubmit = (event: React.FormEvent) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        let Password = PasswordRef.current!.value;
-        let ConfirmPassword = ConfirmPasswordRef.current!.value;
-
-        if (FormHelper.isEmpty(Password)) {
+        if (FormHelper.isEmpty(password)) {
             ToastHelper.errorToast("Password Required")
-        } else if (FormHelper.isEmpty(ConfirmPassword)) {
+        } else if (FormHelper.isEmpty(confirmPassword)) {
             ToastHelper.errorToast("Confirm Password Required")
-        } else if (Password !== ConfirmPassword) {
+        } else if (password !== confirmPassword) {
             ToastHelper.errorToast("Password & Confirm Password Should be Same")
         } else {
             const email = SessionHelper.getEmail();
             const otp = SessionHelper.getOTP();
 
             if (email !== null && otp !== null) {
-                UserRequest.recoverResetPassRequest(email, otp, Password).then((result: boolean) => {
+                setLoading(true);
+                await UserRequest.recoverResetPassRequest(email, otp, password).then((result: boolean) => {
+                    setLoading(false);
                     if (result) {
-                        redirect("/login")
+                        router.push("/login")
                     }
                 })
             }else {
@@ -46,46 +47,60 @@ const CreatePassword: React.FC = () => {
 
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" sx={{paddingTop: '50px'}}>
-                <Grid item xs={10} md={3}>
-                    <Box sx={{textAlign: 'center'}}>
-                        <Typography component="h1" variant="h4">
-                            Set New Password
-                        </Typography>
-                    </Box>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 5, textAlign: 'center'}}>
-                        <TextField
-                            margin="normal"
-                            fullWidth
-                            label="Your Email"
-                            type="email"
-                            defaultValue={SessionHelper.getEmail()}
-                            disabled
-                        />
-                        <TextField
-                            margin="normal"
-                            fullWidth
-                            label="New Password"
-                            type="password"
-                            inputRef={PasswordRef}
-                        />
-                        <TextField
-                            margin="normal"
-                            fullWidth
-                            label="Confirm Password"
-                            type="password"
-                            inputRef={ConfirmPasswordRef}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{mt: 3, mb: 2}}
-                            onClick={handleSubmit}
-                        >
-                            Next
-                        </Button>
-                    </Box>
+            <Grid container justifyContent="center" alignItems="center" sx={{
+                backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                        ? theme.palette.grey[100]
+                        : theme.palette.grey[900],
+                flexGrow: 1,
+                height: '100vh',
+                overflow: 'auto',
+            }} >
+                <Grid item xs={10} xl={4}>
+                    <Card>
+                        <CardContent>
+                            <Box sx={{textAlign: 'center'}}>
+                                <Typography component="h1" variant="h5" sx={{fontWeight : 600, marginBottom : '25px'}}>
+                                    Set New Password
+                                </Typography>
+                            </Box>
+                            <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 5, textAlign: 'center'}}>
+                                <TextField
+                                    margin="normal"
+                                    fullWidth
+                                    label="Your Email"
+                                    type="email"
+                                    defaultValue={SessionHelper.getEmail()}
+                                    disabled
+                                />
+                                <TextField
+                                    margin="normal"
+                                    fullWidth
+                                    label="New Password"
+                                    type="password"
+                                    onChange={(event : ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+                                />
+                                <TextField
+                                    margin="normal"
+                                    fullWidth
+                                    label="Confirm Password"
+                                    type="password"
+                                    onChange={(event : ChangeEvent<HTMLInputElement>) => setConfirmPassword(event.target.value)}
+                                />
+                                <LoadingButton
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{mt: 3, mb: 2}}
+                                    endIcon={<Send />}
+                                    loading={loading}
+                                    loadingPosition="end"
+                                >
+                                    <span>Submit</span>
+                                </LoadingButton>
+                            </Box>
+                        </CardContent>
+                    </Card>
                 </Grid>
             </Grid>
         </>
