f7f8fb5ca81d9a0f94d3f1ba747a29426fe8c6c8
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-30 04:37:19 +0600

    update otp route

--- api/src/routes/otp.router.ts
+++ api/src/routes/otp.router.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import OtpController from "../controllers/otp.controller";
 const otpRouter = express.Router();
 
-otpRouter.get("/recover-verify-email/:email", OtpController.RecoverVerifyEmail);
-otpRouter.get("/recover-verify-otp/:email/:otp", OtpController.RecoverVerifyOTP);
+otpRouter.post("/recover-verify-email", OtpController.RecoverVerifyEmail);
+otpRouter.post("/recover-verify-otp", OtpController.RecoverVerifyOTP);
 otpRouter.post("/recover-reset-pass", OtpController.RecoverResetPass);
 
 export default otpRouter;
