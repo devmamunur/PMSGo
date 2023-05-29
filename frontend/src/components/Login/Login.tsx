fd1c207bec3d82b36f13444a4ac9610578b09666
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-30 00:38:57 +0600

    update login page and add loader button

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,43 +1,45 @@
 "use client"
-import React, {ChangeEvent, useRef, useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import {Card, CardContent, TextField} from "@mui/material";
-import Button from "@mui/material/Button";
+import { LoadingButton } from '@mui/lab';
 import FormHelper from "@/helpers/form.helper";
 import ToastHelper from "@/helpers/toast.helper";
-import Link from "next/link";
+import NextLink from "next/link";
 import {signIn} from "next-auth/react";
 import {useRouter} from "next/navigation";
 
 const Login: React.FC = () => {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const handleSubmit = async  (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
         if (FormHelper.isEmail(email)) {
             ToastHelper.errorToast("Valid Email Address Required !")
         } else if (FormHelper.isEmpty(password)) {
             ToastHelper.errorToast("Password Required !")
         } else {
-
+            setLoading(true);
             const result = await signIn('credentials', {
                 email,
                 password,
                 redirect: false,
-                callbackUrl: '/dashboard',
-            })
-            if(result!.ok){
+            });
+            if (result!.ok) {
+                setLoading(false);
+                ToastHelper.successToast("Successfully SignIn")
                 router.push('/dashboard');
-            }else {
+            } else {
+                setLoading(false);
                 ToastHelper.errorToast('Email or Password Not Match');
             }
         }
-    };
+    }
 
     return (
         <>
@@ -65,7 +67,7 @@ const Login: React.FC = () => {
                                     fullWidth
                                     label="Email Address"
                                     type="email"
-                                    onChange={(event : ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
                                 />
                                 <TextField
                                     margin="normal"
@@ -73,26 +75,28 @@ const Login: React.FC = () => {
                                     fullWidth
                                     label="Password"
                                     type="password"
-                                    onChange={(event : ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
                                 />
-                                <Button
+                                <LoadingButton
                                     type="submit"
                                     fullWidth
+                                    loading={loading}
                                     variant="contained"
-                                    sx={{mt: 3, mb: 2}}
+                                    sx={{mt: 3, mb: 5}}
+                                    loadingPosition="end"
                                 >
-                                    Sign In
-                                </Button>
-                                <Grid container>
+                                    <span>Sign In</span>
+                                </LoadingButton>
+                                <Grid container >
                                     <Grid item xs>
-                                        <Link href="/send-otp">
+                                        <NextLink href="/send-otp">
                                             Forgot password?
-                                        </Link>
+                                        </NextLink>
                                     </Grid>
                                     <Grid item>
-                                        <Link href="/register">
+                                        <NextLink href="/register">
                                             {"Don't have an account? Sign Up"}
-                                        </Link>
+                                        </NextLink>
                                     </Grid>
                                 </Grid>
                             </Box>
