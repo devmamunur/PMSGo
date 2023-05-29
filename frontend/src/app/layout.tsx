30a853cd44ba90b34bd601bbd634effb550cc4f2
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-29 06:07:20 +0600

    update main layout add store provider and axion header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,17 +7,23 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import "react-toastify/dist/ReactToastify.css";
 import '@/assets/css/global.css'
+import {SessionProvider} from "next-auth/react";
+import {RootLayoutPropsWithSession} from "@/interfaces/layout-props.interface";
+import {Provider} from "react-redux";
+import store from "@/redux/store/store";
+import SetAxiosHeaders from "@/components/Axios/SetAxiosHeaders";
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode
-}) {
+export default function RootLayout({children, session}: RootLayoutPropsWithSession) {
     return (
         <html lang="en">
         <body>
-            {children}
+        <Provider store={store}>
+            <SessionProvider session={session}>
+                <SetAxiosHeaders/>
+                {children}
+            </SessionProvider>
             <ToastContainer/>
+        </Provider>
         </body>
         </html>
     )
