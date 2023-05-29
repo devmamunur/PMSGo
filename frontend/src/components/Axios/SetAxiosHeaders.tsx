aa3caabff8f4dbffa4c560e61e8457931fcf294b
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-29 06:06:04 +0600

    made axios header component all inner functionaly

---
+++ frontend/src/components/Axios/SetAxiosHeaders.tsx
@@ -0,0 +1,29 @@
+"use client";
+import {useEffect} from "react";
+import axios from "axios";
+import {useSession} from "next-auth/react";
+
+interface SessionUser {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    accessToken: string;
+}
+
+const SetAxiosHeaders = () => {
+    const {data: session} = useSession()
+    useEffect(() => {
+        console.log("Session data : ", session);
+        if(session) {
+            // @ts-ignore
+            axios.defaults.headers.common = {
+                Authorization : `Bearer ${(session.user as SessionUser).accessToken}`
+            }
+        }
+    }, [session])
+    return null;
+};
+
+export default SetAxiosHeaders;
+
+
