9cf349ca73af18971c3f7a479c8912aede5551b7
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-30 04:18:34 +0600

    remove extra session helper and add removeEmailAndOTP helper

--- frontend/src/helpers/session.helper.ts
+++ frontend/src/helpers/session.helper.ts
@@ -1,49 +1,20 @@
 
 class SessionHelper {
-    static setToken(token: string): void {
-        localStorage.setItem("token", token);
+    static setEmail(email: string): void {
+        localStorage.setItem("email", email)
     }
-
-    static getToken(): any {
-        if (typeof localStorage === 'undefined') {
-            return null;
-        }
-        return localStorage.getItem("token");
-    }
-
-    static setUserDetails(data: any) {
-        if(data){
-            localStorage.setItem("userDetails", JSON.stringify(data));
-        }
-    }
-
-    static getUserDetails(): any {
-        const userDetails = localStorage.getItem("userDetails");
-        if (userDetails === null) {
-            return null;
-        }
-        return JSON.parse(userDetails);
-    }
-
-    static setEmail(Email: string): void {
-        localStorage.setItem("Email", Email)
-    }
-
     static getEmail(): string | null {
-        return localStorage.getItem("Email")
+        return localStorage.getItem("email")
     }
-
     static setOTP(OTP: string): void {
         localStorage.setItem("OTP", OTP)
     }
-
     static getOTP(): string | null {
         return localStorage.getItem("OTP")
     }
-
-    static removeSession(): void {
-        localStorage.clear();
-        window.location.href = '/login'
+    static removeEmailAndOTP(){
+        localStorage.removeItem('email');
+        localStorage.removeItem('OTP');
     }
 }
 
