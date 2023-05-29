f9af0df2bcfb9cef9f7a1812a8f58ed9e53538d7
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-29 06:09:03 +0600

    made auth layout page

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from "react";
+import {useRouter} from "next/navigation";
+import {useSession} from "next-auth/react";
+import {RootLayoutProps} from "@/interfaces/layout-props.interface";
+
+export default function Layout ({ children } : RootLayoutProps) {
+    const router = useRouter();
+    const { status  } = useSession();
+
+    if(status === "loading"){
+        return ("loading...");
+    }else if(status === "authenticated"){
+        router.push('/')
+    }else if(status === "unauthenticated") {
+        return (
+            <>
+                {children}
+            </>
+        )
+    }
+}
