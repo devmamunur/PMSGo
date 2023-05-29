fa31acf17014111aa6f79e36ea35f4291806998f
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-30 04:34:28 +0600

    made otp verify page

---
+++ frontend/src/app/(auth)/verify-otp/page.tsx
@@ -0,0 +1,15 @@
+import React, {lazy, Suspense} from 'react';
+import LazyLoader from "@/components/MasterLayout/LazyLoader";
+
+const VerifyOTP = lazy(() => import('@/components/PasswordRecover/VerifyOTP'));
+const VerifyOtpPage = () => {
+    return (
+        <>
+            <Suspense fallback={<LazyLoader/>}>
+                <VerifyOTP/>
+            </Suspense>
+        </>
+    );
+};
+
+export default VerifyOtpPage;
