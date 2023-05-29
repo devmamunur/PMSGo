eb7f1857e2c1acafea598666060f6ee65ad96c29
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-05-29 06:10:21 +0600

    update  api auth middleware

--- api/src/middleware/auth.middleware.ts
+++ api/src/middleware/auth.middleware.ts
@@ -2,12 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-    const token: string | undefined = req.headers['token'] as string;
-    if (!token) {
+    const authHeader : string | undefined = req.headers['authorization'] as string;
+    if (!authHeader ) {
         return res.status(401).json({ success: false, message: 'No token provided' });
     }
-
-    jwt.verify(token, 'Secret123', (err: any, decoded: any) => {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err: any, decoded: any) => {
         if (err) {
             return res.status(400).json({ success: false, data: err });
         } else {
