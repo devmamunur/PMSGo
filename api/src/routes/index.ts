1bde8caceb7633cc303724b5f6e09ea321258f6c
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-01 06:18:36 +0600

    Add common router configuration file

---
+++ api/src/routes/index.ts
@@ -0,0 +1,14 @@
+import {Application} from "express";
+import userRouter from "./user.route";
+import otpRoute from "./otp.route";
+import taskRouter from "./task.route";
+
+const appRoutes = (app : Application) => {
+    const routes = () => {
+        app.use('/api/v1', userRouter);
+        app.use('/api/v1', otpRoute);
+        app.use('/api/v1', taskRouter);
+    };
+    routes();
+}
+export default appRoutes;
