1859f07b28ac2fef16ede13dd139cc2801ea4b72
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-01 06:19:07 +0600

    some config change

--- api/src/utility/generate-token.utility.ts
+++ api/src/utility/generate-token.utility.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
-import process from "process";
+import {envConfig} from "../config/env.config";
 
 const generateTokenUtility = (payload : any) => {
-    return  jwt.sign(payload, process.env.JWT_SECRET_KEY)
+    return  jwt.sign(payload, envConfig.JWT_SECRET_KEY)
 }
 export default generateTokenUtility;
