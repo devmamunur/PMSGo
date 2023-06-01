820fc6c8a8beef924e513d360b583a4fb12d78b8
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-01 06:18:14 +0600

    update env config

--- api/src/middleware/auth.middleware.ts
+++ api/src/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import {envConfig} from "../config/env.config";
 
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader : string | undefined = req.headers['authorization'] as string;
@@ -7,7 +8,7 @@ const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
         return res.status(401).json({ success: false, message: 'No token provided' });
     }
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err: any, decoded: any) => {
+    jwt.verify(token, envConfig.JWT_SECRET_KEY, (err: any, decoded: any) => {
         if (err) {
             return res.status(400).json({ success: false, data: err });
         } else {
