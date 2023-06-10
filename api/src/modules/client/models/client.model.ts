a35e48919ab34b239ec51794a5fa7bd28460436e
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-11 05:39:20 +0600

    Client Model Create Done

---
+++ api/src/modules/client/models/client.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Model, Schema } from 'mongoose';
+import { ClientInterface } from '../interfaces/client.interface';
+
+const DataSchema: Schema<ClientInterface> = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    email_verified_at: { type: Date, default: null },
+    password: { type: String, required: true },
+    remember_token: { type: String, default: null },
+    type: { type: String, required: true, default: 'client' },
+    currant_workspace: { type: Number, default: null },
+    avatar: { type: String, default: null },
+    address: { type: String, default: null },
+    city: { type: String, default: null },
+    state: { type: String, default: null },
+    zipcode: { type: String, default: null },
+    country: { type: String, default: null },
+    telephone: { type: String, default: null },
+    created_at: { type: Date, default: null },
+    updated_at: { type: Date, default: null }
+  },
+  { versionKey: false }
+);
+
+const ClientModel: Model<ClientInterface> = mongoose.model<ClientInterface>('clients', DataSchema);
+
+export default ClientModel;
