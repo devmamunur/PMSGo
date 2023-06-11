49628d1384c7b2f343cb3f4ed73c5ca0fb17fa11
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-11 06:19:56 +0600

    Made apiVersion variable and reuse it

--- api/src/global/routes/index.ts
+++ api/src/global/routes/index.ts
@@ -4,10 +4,11 @@ import otpRoute from '../../modules/otp/routes/otp.route';
 import taskRouter from '../../modules/task/routes/task.route';
 
 const appRoutes = (app: Application) => {
+  const apiVersion = '/api/v1';
   const routes = () => {
-    app.use('/api/v1', userRouter);
-    app.use('/api/v1', otpRoute);
-    app.use('/api/v1', taskRouter);
+    app.use(apiVersion, userRouter);
+    app.use(apiVersion, otpRoute);
+    app.use(apiVersion, taskRouter);
   };
   routes();
 };
