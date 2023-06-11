d019c39d721d7d39fe2d1d7ed3b9d95541a8cf9c
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-11 06:19:24 +0600

    Plan Interface Create Done

---
+++ api/src/modules/plan/interfaces/plan.interface.ts
@@ -0,0 +1,17 @@
+import { Document, Types } from 'mongoose';
+export interface PlanInterface extends Document {
+  id: number;
+  name: string;
+  monthly_price: number;
+  annual_price: Types.Decimal128;
+  status: number;
+  trial_days: number;
+  max_workspaces: number;
+  max_users: number;
+  max_clients: number;
+  max_projects: number;
+  description?: string | null;
+  image?: string | null;
+  created_at?: Date | null;
+  updated_at?: Date | null;
+}
