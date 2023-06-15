2fcfb524d1b9d9b1a8bd6265da9af77458f3be7e
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-15 09:47:14 +0600

    update signup repoditory

--- api/src/modules/auth/repositories/signup.repository.ts
+++ api/src/modules/auth/repositories/signup.repository.ts
@@ -1,10 +1,10 @@
 import CompanyModel from '../../company/models/company.model';
 import GeneratePasswordUtility from '../../../global/utility/generate-password.utility';
-import {SignupInterface} from '../interfaces/signup.interface';
+import {SignupInterface} from '../interfaces/auth.interface';
 import {ServerError} from '../../../global/utility/error.handler.utility';
 
 class SignupRepository{
-  async signup(reqBody : SignupInterface){
+  async signup(reqBody : SignupInterface) : Promise<void>{
     try{
       reqBody.password = GeneratePasswordUtility(reqBody.password);
       await CompanyModel.create(reqBody);
