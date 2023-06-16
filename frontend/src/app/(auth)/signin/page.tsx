814c4f4034c7617ea2fd7f4b82fb0e9251a78ab4
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-16 11:08:40 +0600

    modify signin page

---
+++ frontend/src/app/(auth)/signin/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React, {lazy, Suspense} from 'react';
+import LazyLoader from "@/components/MasterLayout/LazyLoader";
+const SignIn = lazy(() => import('@/components/SignIn/SignIn'));
+const Login : React.FC = () => {
+    return (
+        <>
+            <Suspense fallback={<LazyLoader/>}>
+                <SignIn/>
+            </Suspense>
+        </>
+    );
+};
+
+export default Login;
