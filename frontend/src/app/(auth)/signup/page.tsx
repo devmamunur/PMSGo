e0bde25110e87d76cbc3422b22dbed5ff5c526e7
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-16 11:08:19 +0600

    modify signup page

---
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, {lazy, Suspense} from 'react';
+import LazyLoader from "@/components/MasterLayout/LazyLoader";
+const SignUp = lazy(() => import('@/components/SignUp/SignUp'));
+const Register : React.FC = () => {
+    return (
+        <>
+            <Suspense fallback={<LazyLoader/>}>
+                <SignUp/>
+            </Suspense>
+        </>
+    );
+};
+export default Register;
