ed05f3797bbba0208b27b904462a628a6550ed04
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-16 11:06:47 +0600

    made login component to signin component and modify signin component  logic

--- frontend/src/components/SignIn/SignIn.tsx
+++ frontend/src/components/SignIn/SignIn.tsx
@@ -4,7 +4,7 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import {Card, CardContent, FormControl, MenuItem, Select, SelectChangeEvent, TextField} from "@mui/material";
-import { LoadingButton } from '@mui/lab';
+import {LoadingButton} from '@mui/lab';
 import FormHelper from "@/helpers/form.helper";
 import ToastHelper from "@/helpers/toast.helper";
 import NextLink from "next/link";
@@ -12,9 +12,12 @@ import {signIn} from "next-auth/react";
 import {useRouter} from "next/navigation";
 import {Login} from "@mui/icons-material";
 import InputLabel from '@mui/material/InputLabel';
+import {authService} from '@/services/api/auth/auth.service';
+import {AxiosError} from 'axios';
+import {ErrorResponse} from '@/interfaces/common';
 
 
-const LoginComponent: React.FC = () => {
+const SignIn: React.FC = () => {
     const router = useRouter();
     const [loading, setLoading] = useState<boolean>(false);
     const [type, setType] = useState<string>("company");
@@ -23,27 +26,41 @@ const LoginComponent: React.FC = () => {
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if(FormHelper.isType(type)){
+        if (FormHelper.isType(type)) {
             ToastHelper.errorToast("User type not valid!")
-        }else if (FormHelper.isEmail(email)) {
+        } else if (FormHelper.isEmail(email)) {
             ToastHelper.errorToast("Valid email address required !")
         } else if (FormHelper.isEmpty(password)) {
             ToastHelper.errorToast("Password required !")
         } else {
             setLoading(true);
             const result = await signIn('credentials', {
+                type,
                 email,
                 password,
                 redirect: false,
             });
             if (result!.ok) {
+                console.log(result);
                 setLoading(false);
                 ToastHelper.successToast("Successfully signIn")
                 router.push('/dashboard');
             } else {
+                console.log(result);
                 setLoading(false);
                 ToastHelper.errorToast('Email or password not match');
             }
+            // try {
+            //     setLoading(true);
+            //     const result = await authService.signin({type, email, password});
+            //     setLoading(false);
+            //     ToastHelper.successToast(result.data.message);
+            //     router.push('/dashboard');
+            // } catch (error) {
+            //     const axiosError = error as AxiosError<ErrorResponse>;
+            //     setLoading(false);
+            //     ToastHelper.errorToast(axiosError?.response?.data?.error);
+            // }
         }
     }
 
@@ -62,27 +79,27 @@ const LoginComponent: React.FC = () => {
                     <Card>
                         <CardContent>
                             <Box sx={{textAlign: 'center'}}>
-                                <Typography component="h1" variant="h5" sx={{fontWeight : 600, marginBottom : '25px'}}>
+                                <Typography component="h1" variant="h5" sx={{fontWeight: 600, marginBottom: '25px'}}>
                                     Sign In
                                 </Typography>
                             </Box>
                             <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
                                 <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-autowidth-label">Sign In As</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-autowidth-label"
-                                    id="demo-simple-select-autowidth"
-                                    required
-                                    fullWidth
-                                    value={type}
-                                    label="Sign In As"
-                                    onChange={(event: SelectChangeEvent) => setType(event.target.value)}
-                                >
-                                    <MenuItem value='admin'>Admin</MenuItem>
-                                    <MenuItem value='company'>Company</MenuItem>
-                                    <MenuItem value='client'>Client</MenuItem>
-                                    <MenuItem value='user'>User</MenuItem>
-                                </Select>
+                                    <InputLabel id="demo-simple-select-autowidth-label">Sign In As</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-autowidth-label"
+                                        id="demo-simple-select-autowidth"
+                                        required
+                                        fullWidth
+                                        value={type}
+                                        label="Sign In As"
+                                        onChange={(event: SelectChangeEvent) => setType(event.target.value)}
+                                    >
+                                        <MenuItem value='admin'>Admin</MenuItem>
+                                        <MenuItem value='company'>Company</MenuItem>
+                                        <MenuItem value='client'>Client</MenuItem>
+                                        <MenuItem value='user'>User</MenuItem>
+                                    </Select>
                                 </FormControl>
                                 <TextField
                                     margin="normal"
@@ -106,12 +123,12 @@ const LoginComponent: React.FC = () => {
                                     loading={loading}
                                     variant="contained"
                                     sx={{mt: 3, mb: 5}}
-                                    endIcon={<Login />}
+                                    endIcon={<Login/>}
                                     loadingPosition="end"
                                 >
                                     <span>Sign In</span>
                                 </LoadingButton>
-                                <Grid container >
+                                <Grid container>
                                     <Grid item xs>
                                         <NextLink href="/send-otp">
                                             Forgot password?
@@ -132,4 +149,4 @@ const LoginComponent: React.FC = () => {
     );
 };
 
-export default LoginComponent;
+export default SignIn;
