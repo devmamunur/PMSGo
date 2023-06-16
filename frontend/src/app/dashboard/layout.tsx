4c4e9f9523970d42e14cf1e0a9caee8a7b0c52fd
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-16 11:07:50 +0600

    update dashboard component

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -5,17 +5,21 @@ import {useRouter} from "next/navigation";
 import {RootLayoutProps} from "@/interfaces/layout-props.interface";
 export default function Layout ({ children } : RootLayoutProps) {
     const router = useRouter();
-    const { status  } = useSession();
-    console.log('Status : '+ status);
-    if(status === "loading"){
-        return ("loading...");
-    }else if(status === "authenticated"){
-        return (
-            <>
-                {children}
-            </>
-        )
-    }else if(status === "unauthenticated") {
-        router.push('login')
+    const { status } = useSession({
+        required: true,
+        onUnauthenticated() {
+            router.push('signin')
+        },
+    })
+
+    if (status === "loading") {
+        return "Loading or not authenticated..."
     }
+
+    return (
+        <>
+            {children}
+        </>
+    )
+
 }
