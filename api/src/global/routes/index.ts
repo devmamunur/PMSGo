3412fc25db5f15a5bbb6e59400ea23afd88ecf78
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-18 04:03:17 +0600

    add middleware in base route

--- api/src/global/routes/index.ts
+++ api/src/global/routes/index.ts
@@ -1,5 +1,7 @@
 import { Application } from 'express';
 import {authRouter} from '../../modules/auth/routes/auth.route';
+import {authMiddleware} from '../middleware/auth.middleware';
+import {userRouter} from '../../modules/user/routes/user.route';
 
 const apiVersion = '/api/v1';
 
@@ -7,6 +9,7 @@ const appRoutes = (app: Application) => {
 
   const routes = () => {
     app.use(apiVersion, authRouter.routes());
+    app.use(apiVersion, authMiddleware.checkAuth, userRouter.routes());
   };
   routes();
 };
