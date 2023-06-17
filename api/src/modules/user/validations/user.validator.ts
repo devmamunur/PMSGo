0bf2fa366b6a289ac8c8ccde8395a6a9c7e63f85
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-17 23:57:55 +0600

    remove unused validation code

--- api/src/modules/user/validations/user.validator.ts
+++ api/src/modules/user/validations/user.validator.ts
@@ -1,7 +1,5 @@
 import Joi from 'joi';
 import {Request} from 'express';
-import CompanyModel from '../../company/models/company.model';
-import {JoiRequestValidationError} from '../../../global/utility/error.handler.utility';
 import UserModel from '../models/user.model';
 
 class UserValidator{
@@ -24,60 +22,3 @@ class UserValidator{
 }
 
 export const userValidator : UserValidator = new UserValidator();
-
-
-
-
-
-
-
-
-
-
-// *******************************
-// I Will Rewrite All those Code
-// ******************************
-
-// import { Request } from 'express';
-// import CompanyModel from '../../company/models/company.model';
-// import UserModel from '../models/user.model';
-//
-// const Joi = require('joi');
-//
-// class UserValidator {
-//   static registerSchema = Joi.object({
-//     email: Joi.string().email().required(),
-//     firstName: Joi.string().required(),
-//     lastName: Joi.string().required(),
-//     organization: Joi.string().alphanum().required(),
-//     mobile: Joi.number().min(10).required(),
-//     password: Joi.string().min(6).trim(true).required(),
-//     photo: Joi.string()
-//   });
-//
-//   static loginSchema = Joi.object({
-//     email: Joi.string().email().required(),
-//     password: Joi.string().min(6).trim(true).required()
-//   });
-//
-//   static async validateUniqueEmail(resBody: Request['body']) {
-//     const { email } = resBody;
-//     const existingEmail = await UserModel.findOne({ email: email });
-//     if (existingEmail) {
-//       throw new Error('Email must be unique');
-//     }
-//   }
-//
-//
-//   static async registerValidation(resBody: Request['body']) {
-//     await this.validateUniqueEmail(resBody);
-//     await this.registerSchema.validateAsync(resBody);
-//     await this.validateUniqueOrganizationName(resBody);
-//   }
-//
-//   static async loginValidation(resBody: Request['body']) {
-//     await this.loginSchema.validateAsync(resBody);
-//   }
-// }
-//
-// export default UserValidator;
