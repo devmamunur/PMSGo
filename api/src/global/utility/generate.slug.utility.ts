4a6e6c2173b092de24ffad1bbbfa7f9029dfe5e4
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-21 05:28:28 +0600

    made slug generator utility function

---
+++ api/src/global/utility/generate.slug.utility.ts
@@ -0,0 +1,10 @@
+const generateSlug = (title) => {
+  return title
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
+    .trim();
+};
+
+export default generateSlug;
