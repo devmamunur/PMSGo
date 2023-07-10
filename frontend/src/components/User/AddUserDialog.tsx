1464d97de0dca9eef21f1363bf957e609958b1ec
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-10 23:08:18 +0600

    Add form and form input in AddUserDialog component

--- frontend/src/components/User/AddUserDialog.tsx
+++ frontend/src/components/User/AddUserDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -7,6 +7,10 @@ import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
+import {Box, Grid, TextField} from '@mui/material';
+import {LoadingButton} from '@mui/lab';
+import {LockOpen, Save} from '@mui/icons-material';
+import NextLink from 'next/link';
 
 
 export interface DialogTitleProps {
@@ -44,8 +48,18 @@ interface AddUserDialogProps {
     open : boolean
 }
 const AddUserDialog : React.FC<AddUserDialogProps> = ({clickDialog, open}) => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [company, setCompany] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
     const handleClose = () => {
         clickDialog()
+    };
+
+    const handleSubmit = () => {
+
     };
     return (
         <>
@@ -58,28 +72,29 @@ const AddUserDialog : React.FC<AddUserDialogProps> = ({clickDialog, open}) => {
                     Add user
                 </BootstrapDialogTitle>
                 <DialogContent dividers>
-                    <Typography gutterBottom>
-                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                        consectetur ac, vestibulum at eros.
-                    </Typography>
-                    <Typography gutterBottom>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-                        Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-                    </Typography>
-                    <Typography gutterBottom>
-                        Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-                        magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-                        ullamcorper nulla non metus auctor fringilla.
-                    </Typography>
+                    <Box component="form" noValidate sx={{mt: 1}}>
+                        <TextField margin="normal" fullWidth label="Full Name"
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)}/>
+                        <TextField margin="normal" fullWidth label="Email Address" type="email"
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}/>
+                        <TextField margin="normal" fullWidth label="Password" type="password"
+                                   onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}/>
+                    </Box>
                 </DialogContent>
-                <DialogActions>
+                <DialogActions sx={{paddingTop : "14px", paddingBottom : "14px"}}>
                     <Button onClick={handleClose}>
                         Close
                     </Button>
-                    <Button>
-                        Save
-                    </Button>
+                    <LoadingButton
+                        onClick={handleSubmit}
+                        type="submit"
+                        variant="contained"
+                        loading={loading}
+                        endIcon={<Save/>}
+                        loadingPosition="end"
+                    >
+                        <span>Save</span>
+                    </LoadingButton>
                 </DialogActions>
             </Dialog>
         </>
