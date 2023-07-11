a54feabc3d8ea3dd92c32f22a10b52ae90130771
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-12 03:47:04 +0600

    Made axios provider

---
+++ frontend/src/services/AxiosProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, {ReactNode, useEffect} from "react";
+import axiosInstance from '@/services/axios';
+import {useSession} from 'next-auth/react';
+
+interface Props {
+    children: ReactNode;
+}
+function AxiosProvider({ children }: Props) {
+    const { data: session } = useSession();
+    useEffect(() => {
+        if (session && session.user && (session.user as { accessToken: string }).accessToken) {
+                axiosInstance.interceptors.request.use(async (config) => {
+                config.headers['Authorization'] = `Bearer ${(session.user as { accessToken: string }).accessToken}`;
+                return config;
+            });
+        }
+    }, [session]);
+    return <>{children}</>;
+}
+
+export default AxiosProvider;
