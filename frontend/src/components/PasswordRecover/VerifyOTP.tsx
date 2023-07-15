c6ca04955a1c853b35d29e19b0644d9c8250deff
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-15 22:42:51 +0600

    format files

--- frontend/src/components/PasswordRecover/VerifyOTP.tsx
+++ frontend/src/components/PasswordRecover/VerifyOTP.tsx
@@ -1,91 +1,107 @@
-"use client"
-import React, {useState} from 'react';
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+'use client';
+import React, { useState } from 'react';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { LoadingButton } from '@mui/lab';
-import UserRequest from "@/APIRequests/user.request";
-import {useRouter} from "next/navigation";
-import ToastHelper from "@/helpers/toast.helper";
-import SessionHelper from "@/helpers/session.helper";
-import {MuiOtpInput} from 'mui-one-time-password-input'
-import {Card, CardContent} from "@mui/material";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-
+import UserRequest from '@/APIRequests/user.request';
+import { useRouter } from 'next/navigation';
+import ToastHelper from '@/helpers/toast.helper';
+import SessionHelper from '@/helpers/session.helper';
+import { MuiOtpInput } from 'mui-one-time-password-input';
+import { Card, CardContent } from '@mui/material';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const VerifyOTP: React.FC = () => {
-    const router = useRouter();
-    const [loading, setLoading] = useState<boolean>(false);
-    const [OTP, setOTP] = useState<any>();
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        if (OTP.length === 6) {
-            const email = SessionHelper.getEmail();
-            if (email !== null) {
-                setLoading(true);
-                await UserRequest.recoverVerifyOTPRequest(email, OTP).then((result: boolean) => {
-                    setLoading(false);
-                    if (result) {
-                        router.push("/create-password");
-                    }
-                });
-            } else {
-                ToastHelper.errorToast("OTP Verify Failed!");
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [OTP, setOTP] = useState<any>();
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (OTP.length === 6) {
+      const email = SessionHelper.getEmail();
+      if (email !== null) {
+        setLoading(true);
+        await UserRequest.recoverVerifyOTPRequest(email, OTP).then(
+          (result: boolean) => {
+            setLoading(false);
+            if (result) {
+              router.push('/create-password');
             }
-        } else {
-            ToastHelper.errorToast("Enter 6 Digit Code");
-        }
-    };
-    const handleOTPChange = (value: string) => {
-        setOTP(value);
-    };
-    return (
-        <>
-            <Grid container justifyContent="center" alignItems="center" sx={{
-                backgroundColor: (theme) =>
-                    theme.palette.mode === 'light'
-                        ? theme.palette.grey[100]
-                        : theme.palette.grey[900],
-                flexGrow: 1,
-                height: '100vh',
-                overflow: 'auto',
-            }}>
-                <Grid item xs={10} xl={4}>
-                    <Card>
-                        <CardContent>
-                            <Box sx={{textAlign: 'center'}}>
-                                <Typography component="h1" variant="h5" sx={{fontWeight : 600, marginBottom : '25px'}}>
-                                    OTP Verification
-                                </Typography>
-                                <Typography component="p" variant="body1">
-                                    A 6 Digit verification code has been sent to your email address.
-                                </Typography>
-                            </Box>
-                            <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 5, textAlign: 'center'}}>
-                                <MuiOtpInput
-                                    value={OTP}
-                                    onChange={handleOTPChange}
-                                    length={6}
-                                    validateChar={(character: string, index: number) => true}
-                                />
-                                <LoadingButton
-                                    type="submit"
-                                    fullWidth
-                                    variant="contained"
-                                    endIcon={<KeyboardArrowRightIcon />}
-                                    sx={{mt: 3, mb: 2}}
-                                    loading={loading}
-                                    loadingPosition="end"
-                                >
-                                    <span>Next</span>
-                                </LoadingButton>
-                            </Box>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
-        </>
-    );
+          }
+        );
+      } else {
+        ToastHelper.errorToast('OTP Verify Failed!');
+      }
+    } else {
+      ToastHelper.errorToast('Enter 6 Digit Code');
+    }
+  };
+  const handleOTPChange = (value: string) => {
+    setOTP(value);
+  };
+  return (
+    <>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          backgroundColor: theme =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <Grid item xs={10} xl={4}>
+          <Card>
+            <CardContent>
+              <Box sx={{ textAlign: 'center' }}>
+                <Typography
+                  component="h1"
+                  variant="h5"
+                  sx={{ fontWeight: 600, marginBottom: '25px' }}
+                >
+                  OTP Verification
+                </Typography>
+                <Typography component="p" variant="body1">
+                  A 6 Digit verification code has been sent to your email
+                  address.
+                </Typography>
+              </Box>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 5, textAlign: 'center' }}
+              >
+                <MuiOtpInput
+                  value={OTP}
+                  onChange={handleOTPChange}
+                  length={6}
+                  validateChar={(character: string, index: number) => true}
+                />
+                <LoadingButton
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  endIcon={<KeyboardArrowRightIcon />}
+                  sx={{ mt: 3, mb: 2 }}
+                  loading={loading}
+                  loadingPosition="end"
+                >
+                  <span>Next</span>
+                </LoadingButton>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default VerifyOTP;
--- frontend/src/components/User/AddUserDialog.tsx
+++ frontend/src/components/User/AddUserDialog.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -7,124 +7,146 @@ import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
-import {Box, Grid, TextField} from '@mui/material';
-import {LoadingButton} from '@mui/lab';
-import {LockOpen, Save} from '@mui/icons-material';
+import { Box, Grid, TextField } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { LockOpen, Save } from '@mui/icons-material';
 import NextLink from 'next/link';
-import {useSession} from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import FormHelper from '@/helpers/form.helper';
 import ToastHelper from '@/helpers/toast.helper';
-import {authService} from '@/services/api/auth/auth.service';
-import {AxiosError} from 'axios';
-import {ErrorResponse} from '@/interfaces/common';
-import {userService} from '@/services/api/user/user.service';
-
+import { authService } from '@/services/api/auth/auth.service';
+import { AxiosError } from 'axios';
+import { ErrorResponse } from '@/interfaces/common';
+import { userService } from '@/services/api/user/user.service';
 
 export interface DialogTitleProps {
-    id: string;
-    children?: React.ReactNode;
-    onClose: () => void;
+  id: string;
+  children?: React.ReactNode;
+  onClose: () => void;
 }
 
 function BootstrapDialogTitle(props: DialogTitleProps) {
-    const { children, onClose, ...other } = props;
+  const { children, onClose, ...other } = props;
 
-    return (
-        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-            {children}
-            {onClose ? (
-                <IconButton
-                    aria-label="close"
-                    onClick={onClose}
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            ) : null}
-        </DialogTitle>
-    );
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: theme => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </DialogTitle>
+  );
 }
 
 interface AddUserDialogProps {
-    clickDialog: () => void;
-    open : boolean
+  clickDialog: () => void;
+  open: boolean;
 }
-const AddUserDialog : React.FC<AddUserDialogProps> = ({clickDialog, open}) => {
-    const [loading, setLoading] = useState<boolean>(false);
-    const [company, setCompany] = useState<string>("");
-    const [name, setName] = useState<string>("");
-    const [email, setEmail] = useState<string>("d");
-    const [password, setPassword] = useState<string>("");
-    const { data: session } = useSession();
+const AddUserDialog: React.FC<AddUserDialogProps> = ({ clickDialog, open }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [company, setCompany] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('d');
+  const [password, setPassword] = useState<string>('');
+  const { data: session } = useSession();
 
-    useEffect(() => {
-        if (session && session.user && (session.user as { _id: string })._id) {
-            setCompany((session.user as { _id: string })._id);
-        }
-    }, [session]);
-    const handleClose = () => {
-        clickDialog()
-    };
+  useEffect(() => {
+    if (session && session.user && (session.user as { _id: string })._id) {
+      setCompany((session.user as { _id: string })._id);
+    }
+  }, [session]);
+  const handleClose = () => {
+    clickDialog();
+  };
 
-    const handleSubmit = async () => {
-        if(FormHelper.isEmpty(name)){
-            ToastHelper.errorToast('Name required!');
-        }else if(FormHelper.isEmail(email)){
-            ToastHelper.errorToast('Valid email address required!');
-        }else if(FormHelper.isEmpty(password)){
-            ToastHelper.errorToast('Password required!');
-        }else {
-            setLoading(true);
-            await userService.create({company, name, email, password}).then((res : boolean) => {
-                if(res){
-                    handleClose();
-                }
-                setLoading(false);
-            });
-        }
-    };
-    return (
-        <>
-            <Dialog
-                onClose={handleClose}
-                aria-labelledby="customized-dialog-title"
-                open={open}
-            >
-                <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
-                    Add user
-                </BootstrapDialogTitle>
-                <DialogContent dividers>
-                    <Box component="form" noValidate sx={{mt: 1}}>
-                        <TextField margin="normal" fullWidth label="Full Name"
-                                   onChange={(event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)}/>
-                        <TextField margin="normal" fullWidth label="Email Address" type="email"
-                                   onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}/>
-                        <TextField margin="normal" fullWidth label="Password" type="password"
-                                   onChange={(event: ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}/>
-                    </Box>
-                </DialogContent>
-                <DialogActions sx={{paddingTop : "14px", paddingBottom : "14px"}}>
-                    <Button onClick={handleClose}>
-                        Close
-                    </Button>
-                    <LoadingButton
-                        onClick={handleSubmit}
-                        variant="contained"
-                        loading={loading}
-                        endIcon={<Save/>}
-                        loadingPosition="end"
-                    >
-                        <span>Save</span>
-                    </LoadingButton>
-                </DialogActions>
-            </Dialog>
-        </>
-    );
+  const handleSubmit = async () => {
+    if (FormHelper.isEmpty(name)) {
+      ToastHelper.errorToast('Name required!');
+    } else if (FormHelper.isEmail(email)) {
+      ToastHelper.errorToast('Valid email address required!');
+    } else if (FormHelper.isEmpty(password)) {
+      ToastHelper.errorToast('Password required!');
+    } else {
+      setLoading(true);
+      await userService
+        .create({ company, name, email, password })
+        .then((res: boolean) => {
+          if (res) {
+            handleClose();
+          }
+          setLoading(false);
+        });
+    }
+  };
+  return (
+    <>
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <BootstrapDialogTitle
+          id="customized-dialog-title"
+          onClose={handleClose}
+        >
+          Add user
+        </BootstrapDialogTitle>
+        <DialogContent dividers>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              fullWidth
+              label="Full Name"
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setName(event.target.value)
+              }
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              label="Email Address"
+              type="email"
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setEmail(event.target.value)
+              }
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              label="Password"
+              type="password"
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setPassword(event.target.value)
+              }
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions sx={{ paddingTop: '14px', paddingBottom: '14px' }}>
+          <Button onClick={handleClose}>Close</Button>
+          <LoadingButton
+            onClick={handleSubmit}
+            variant="contained"
+            loading={loading}
+            endIcon={<Save />}
+            loadingPosition="end"
+          >
+            <span>Save</span>
+          </LoadingButton>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 };
 
 export default AddUserDialog;
