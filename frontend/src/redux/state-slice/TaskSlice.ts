3e8e60ad2de6ab7e2d53efd317566f061da2d8b1
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-15 22:41:45 +0600

    format files

--- frontend/src/redux/state-slice/TaskSlice.ts
+++ frontend/src/redux/state-slice/TaskSlice.ts
@@ -1,55 +1,55 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface TaskState {
-    New: any;
-    Completed: any;
-    Progress: any;
-    Canceled: any;
-    Total: any;
-    ALLTask: any;
+  New: any;
+  Completed: any;
+  Progress: any;
+  Canceled: any;
+  Total: any;
+  ALLTask: any;
 }
 
 const initialState: TaskState = {
-    New: [],
-    Completed: [],
-    Progress: [],
-    Canceled: [],
-    Total: [],
-    ALLTask: [],
+  New: [],
+  Completed: [],
+  Progress: [],
+  Canceled: [],
+  Total: [],
+  ALLTask: [],
 };
 
 export const taskSlice = createSlice({
-    name: 'task',
-    initialState,
-    reducers: {
-        setNewTask: (state, action: PayloadAction<any[]>) => {
-            state.New = action.payload;
-        },
-        setCompletedTask: (state, action: PayloadAction<any[]>) => {
-            state.Completed = action.payload;
-        },
-        setProgressTask: (state, action: PayloadAction<any[]>) => {
-            state.Progress = action.payload;
-        },
-        setCanceledTask: (state, action: PayloadAction<any[]>) => {
-            state.Canceled = action.payload;
-        },
-        setTotal: (state, action: PayloadAction<any[]>) => {
-            state.Total = action.payload;
-        },
-        setAllTask: (state, action: PayloadAction<any[]>) => {
-            state.ALLTask = action.payload;
-        },
+  name: 'task',
+  initialState,
+  reducers: {
+    setNewTask: (state, action: PayloadAction<any[]>) => {
+      state.New = action.payload;
     },
+    setCompletedTask: (state, action: PayloadAction<any[]>) => {
+      state.Completed = action.payload;
+    },
+    setProgressTask: (state, action: PayloadAction<any[]>) => {
+      state.Progress = action.payload;
+    },
+    setCanceledTask: (state, action: PayloadAction<any[]>) => {
+      state.Canceled = action.payload;
+    },
+    setTotal: (state, action: PayloadAction<any[]>) => {
+      state.Total = action.payload;
+    },
+    setAllTask: (state, action: PayloadAction<any[]>) => {
+      state.ALLTask = action.payload;
+    },
+  },
 });
 
 export const {
-    setNewTask,
-    setCompletedTask,
-    setProgressTask,
-    setCanceledTask,
-    setTotal,
-    setAllTask,
+  setNewTask,
+  setCompletedTask,
+  setProgressTask,
+  setCanceledTask,
+  setTotal,
+  setAllTask,
 } = taskSlice.actions;
 
 export default taskSlice.reducer;
