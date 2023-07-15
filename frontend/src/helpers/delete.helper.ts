5b35b217e16a5e07cb6c738fd92f47d750b1f9f5
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-15 22:42:30 +0600

    format files

--- frontend/src/helpers/delete.helper.ts
+++ frontend/src/helpers/delete.helper.ts
@@ -1,41 +1,41 @@
-import Swal from "sweetalert2";
-import TaskRequest from "@/APIRequests/task.request";
+import Swal from 'sweetalert2';
+import TaskRequest from '@/APIRequests/task.request';
 
 class DeleteHelper {
-    static deleteToDO(id : string){
-        return  Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                return  TaskRequest.deleteTask(id).then((res : any)=>{
-                    return res
-                })
-            }
-        })
-    }
-    static deleteSelectedTaskAlert(ids : any){
-        return  Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                return  TaskRequest.deleteSelectedTask(ids).then((res : any)=>{
-                    return res
-                })
-            }
-        })
-    }
+  static deleteToDO(id: string) {
+    return Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(result => {
+      if (result.isConfirmed) {
+        return TaskRequest.deleteTask(id).then((res: any) => {
+          return res;
+        });
+      }
+    });
+  }
+  static deleteSelectedTaskAlert(ids: any) {
+    return Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(result => {
+      if (result.isConfirmed) {
+        return TaskRequest.deleteSelectedTask(ids).then((res: any) => {
+          return res;
+        });
+      }
+    });
+  }
 }
 
 export default DeleteHelper;
--- frontend/src/helpers/form.helper.ts
+++ frontend/src/helpers/form.helper.ts
@@ -1,27 +1,27 @@
-import { toast  } from 'react-toastify';
+import { toast } from 'react-toastify';
 let EmailRegx = /\S+@\S+\.\S+/;
 const validTypes = ['company', 'user', 'client', 'admin'];
 class FormHelper {
-    static isEmpty(value : any) : boolean {
-        return value.length === 0;
-    }
-    static isMobile(value : any): boolean{
-        return value > 9;
-    }
-    static isEmail(value : any): boolean {
-        return !EmailRegx.test(value);
-    }
-    static isType(value : any){
-      return  !validTypes.includes(value);
-    }
-    static getBase64(file : any) : any {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = (error) => reject(error);
-        });
-    }
+  static isEmpty(value: any): boolean {
+    return value.length === 0;
+  }
+  static isMobile(value: any): boolean {
+    return value > 9;
+  }
+  static isEmail(value: any): boolean {
+    return !EmailRegx.test(value);
+  }
+  static isType(value: any) {
+    return !validTypes.includes(value);
+  }
+  static getBase64(file: any): any {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  }
 }
 
 export default FormHelper;
