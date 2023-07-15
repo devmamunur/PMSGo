17de8c7812e741f1aef5c6224dd01c0bee96a104
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-15 22:41:34 +0600

    format files

--- frontend/src/services/api/auth/auth.service.ts
+++ frontend/src/services/api/auth/auth.service.ts
@@ -1,13 +1,13 @@
 import axios from '@/services/axios';
-import {SignUpInterface} from '@/interfaces/auth/auth.interface';
+import { SignUpInterface } from '@/interfaces/auth/auth.interface';
 
-class AuthService{
-    async signup(body :SignUpInterface){
-        return await axios.post('/signup', body);
-    }
-    async signin(body :SignUpInterface){
-        return await axios.post('/signin', body);
-    }
+class AuthService {
+  async signup(body: SignUpInterface) {
+    return await axios.post('/signup', body);
+  }
+  async signin(body: SignUpInterface) {
+    return await axios.post('/signin', body);
+  }
 }
 
-export const authService : AuthService = new AuthService();
+export const authService: AuthService = new AuthService();
