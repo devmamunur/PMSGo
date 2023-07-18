1bdad82fb2cb05d10ef733c23e080982e93f64cb
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-19 01:05:55 +0600

    add project route in main route

--- api/src/global/routes/index.ts
+++ api/src/global/routes/index.ts
@@ -2,6 +2,7 @@ import { Application } from 'express';
 import {authRouter} from '../../modules/auth/routes/auth.route';
 import {authMiddleware} from '../middleware/auth.middleware';
 import {userRouter} from '../../modules/user/routes/user.route';
+import {projectRouter} from '../../modules/project/routes/project.route';
 
 const apiVersion = '/api/v1';
 
@@ -10,6 +11,7 @@ const appRoutes = (app: Application) => {
   const routes = () => {
     app.use(apiVersion, authRouter.routes());
     app.use(apiVersion, authMiddleware.checkAuth, userRouter.routes());
+    app.use(apiVersion, authMiddleware.checkAuth, projectRouter.routes());
   };
   routes();
 };
