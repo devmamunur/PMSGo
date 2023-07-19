e4049b0bd86b22bf3c977bebdb8653a8c12747a1
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-19 10:40:33 +0600

    made getCompanyCurrantWorkspaceId method for get company workspace id by company id

--- api/src/modules/company/services/company.service.ts
+++ api/src/modules/company/services/company.service.ts
@@ -1,6 +1,7 @@
 import {SignupInterface} from '../../auth/interfaces/auth.interface';
 import {Types} from 'mongoose';
 import CompanyModel from '../models/company.model';
+import {Request} from 'express';
 
 class CompanyService{
   async createCompanyGetId(reqBody: SignupInterface, defaultPlanId: Types.ObjectId) {
@@ -23,6 +24,29 @@ class CompanyService{
     );
     if(!updatedUser){
       throw new Error('User plan update failed');
+    }
+  }
+
+  async getCompanyCurrantWorkspaceId(req : Request) : Promise<Types.ObjectId>{
+    const userId = req.currentUser._id;
+    const company = await CompanyModel.aggregate(
+      [
+        {
+          $match: {
+            _id: new Types.ObjectId(userId),
+          },
+        },
+        {
+          $project : {
+            currant_workspace : 1,
+          }
+        }
+      ]
+    );
+    if(company && company.length > 0){
+      return company[0].currant_workspace;
+    }else {
+      throw new Error('Company default workspace not available');
     }
   }
 }
