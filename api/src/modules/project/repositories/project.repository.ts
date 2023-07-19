391822c6e458e2fa73ab369520d6b1a11f7a16d9
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-19 10:39:05 +0600

    project create repository all functionality  done

--- api/src/modules/project/repositories/project.repository.ts
+++ api/src/modules/project/repositories/project.repository.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from 'express';
 import {projectValidator} from '../validations/project.validator';
 import {projectService} from '../services/project.service';
+import {userService} from '../../user/services/user.service';
+import {companyService} from '../../company/services/company.service';
 
 class ProjectRepository{
   async create(req : Request, res: Response) : Promise<void>{
     try {
       await projectValidator.createValidate(req.body);
-      const projectId = await projectService.createProjectGetId(req.body);
+      const companyCurrantWorkspaceId = await companyService.getCompanyCurrantWorkspaceId(req);
+      const projectId = await projectService.createProjectGetId(req, companyCurrantWorkspaceId);
+      await projectService.createUserProject(req.body, projectId);
+      res.status(200).json({message : 'Project create successfully'});
     }catch (error){
       res.status(400).json({error : error.message});
     }
