2ed88bc702ee55c26f7211838ca20937f7f03a81
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-22 23:58:15 +0600

    Made getSingleProject method for controller

--- api/src/modules/project/controllers/project.controller.ts
+++ api/src/modules/project/controllers/project.controller.ts
@@ -2,11 +2,14 @@ import {Request, Response} from 'express';
 import {projectRepository} from '../repositories/project.repository';
 
 class ProjectController{
+  async create(req: Request, res: Response) : Promise<void>{
+    await projectRepository.create(req, res);
+  }
   async get(req: Request, res: Response) : Promise<void>{
     await projectRepository.get(req, res);
   }
-  async create(req: Request, res: Response) : Promise<void>{
-    await projectRepository.create(req, res);
+  async getSingleProject(req: Request, res: Response): Promise<void>{
+    await projectRepository.getSingleProject(req, res);
   }
 }
 
