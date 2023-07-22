4263428c83089fbbf0c22bbfd8f09ff094b83b82
Mamunur Rashid <devmamunur1@gmail.com>
2023-07-22 23:41:48 +0600

    update project model

--- api/src/modules/project/models/project.model.ts
+++ api/src/modules/project/models/project.model.ts
@@ -9,7 +9,7 @@ const DataSchema: Schema<ProjectInterface> = new Schema(
     start_date: { type: Date, default: null },
     end_date: { type: Date, default: null },
     budget: { type: Number, default: 0 },
-    workspace: { type: Schema.Types.ObjectId, required: true },
+    workspace: { type: Schema.Types.ObjectId, ref: 'workspaces', required: true },
     created_by: { type: Schema.Types.ObjectId, ref: 'companies', required: true },
     is_active: { type: Number, required: true, default: 1 },
     created_at: { type: Date, default: null },
