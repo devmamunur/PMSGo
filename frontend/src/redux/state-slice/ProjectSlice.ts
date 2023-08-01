1e4cd9e00f9b30fbe3bcfc9d3fbe5687ce89eb4e
Mamunur Rashid <devmamunur1@gmail.com>
2023-08-02 03:47:06 +0600

    Added project add and update global state and action

--- frontend/src/redux/state-slice/ProjectSlice.ts
+++ frontend/src/redux/state-slice/ProjectSlice.ts
@@ -2,10 +2,14 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface ProjectState {
     value: any[];
+    projectAddDialog : boolean;
+    projectEditDialog : boolean;
 }
 
 const initialState: ProjectState = {
     value: [],
+    projectAddDialog: false,
+    projectEditDialog: false,
 };
 export const projectSlice = createSlice({
     name: 'projects',
@@ -14,7 +18,13 @@ export const projectSlice = createSlice({
         setProject: (state, action: PayloadAction<any[]>) => {
             state.value = action.payload;
         },
+        setProjectAddDialog: (state, action: PayloadAction<boolean>) => {
+            state.projectAddDialog = action.payload;
+        },
+        setProjectEditDialog: (state, action: PayloadAction<boolean>) => {
+            state.projectEditDialog = action.payload;
+        },
     },
 });
-export const { setProject } = projectSlice.actions;
+export const { setProject, setProjectAddDialog, setProjectEditDialog } = projectSlice.actions;
 export default projectSlice.reducer;
