68e1189407c0f836ed937faab9c59ef9178c0841
Mamunur Rashid <devmamunur1@gmail.com>
2023-08-02 03:49:18 +0600

    made add update dialog

---
+++ frontend/src/components/Project/AddProjectDialog.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProjectAddUpdate from '@/components/Project/ProjectAddUpdate';
+
+
+const AddProjectDialog: React.FC = () => {
+
+
+  return (
+    <>
+      <ProjectAddUpdate/>
+    </>
+  );
+};
+
+export default AddProjectDialog;
