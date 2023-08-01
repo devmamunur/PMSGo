a3eba3bea39a829190bb641a80e8df1a313e034b
Mamunur Rashid <devmamunur1@gmail.com>
2023-08-02 03:48:44 +0600

    made project edit dialog

---
+++ frontend/src/components/Project/EditProjectDialog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectAddUpdate from '@/components/Project/ProjectAddUpdate';
+
+const EditProjectDialog: React.FC = () => {
+  return (
+    <>
+      <ProjectAddUpdate/>
+    </>
+  );
+};
+
+export default EditProjectDialog;
