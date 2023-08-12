10521e26cd064883ccfaf85c25b67b92c441aa61
Mamunur Rashid <devmamunur1@gmail.com>
2023-08-12 16:04:09 +0600

    made project interface

--- frontend/src/interfaces/project/project.interface.ts
+++ frontend/src/interfaces/project/project.interface.ts
@@ -9,3 +9,11 @@ export interface ProjectCreateInterface {
     start_date: Dayjs | null;
     end_date: Dayjs | null;
 }
+export interface ProjectUpdateInterface {
+    name: string;
+    status: string;
+    budget: number;
+    description: string;
+    start_date: Dayjs | null;
+    end_date: Dayjs | null;
+}
