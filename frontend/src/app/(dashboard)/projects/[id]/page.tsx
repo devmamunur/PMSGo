e05a69aa1926359b8844af64060269fca7197cac
Mamunur Rashid <devmamunur1@gmail.com>
2023-08-21 00:00:39 +0600

    made project details page

--- frontend/src/app/(dashboard)/projects/[id]/page.tsx
+++ frontend/src/app/(dashboard)/projects/[id]/page.tsx
@@ -1,13 +1,14 @@
-import React, {Suspense} from 'react';
+
+import React, {lazy, Suspense} from 'react';
 import MasterLayout from '@/components/MasterLayout/MasterLayout';
 import LazyLoader from '@/components/MasterLayout/LazyLoader';
-
+const ProjectDetails = lazy(() => import('@/components/Project/ProjectDetails'));
 const ProjectDetailsPage = () => {
     return (
         <>
             <MasterLayout>
                 <Suspense fallback={<LazyLoader />}>
-                    Project detail
+                    <ProjectDetails/>
                 </Suspense>
             </MasterLayout>
         </>
